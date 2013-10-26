853f14862fb12bc174414a063a5d3237dda31796
Robert Feldt <robert.feldt@gmail.com>
2013-10-26 07:08:49 +0200

    added optimization of more functions

--- test/problems/test_optimize_single_objective_problems.jl
+++ test/problems/test_optimize_single_objective_problems.jl
@@ -15,27 +15,21 @@ function de_fitness_for(problem, numDimensions, populationSize, numSteps)
   fitness
 end
 
-facts("Optimize sphere in 5 dimensions with DE") do
-  p = GlobalOptim.Problems.examples["Sphere"]
-  @fact de_fitness_for(p, 5, 20, 2000) < 0.01 => true
-end
+facts("Optimize single objective problems in 5, 10, and 30 dimensions with DE") do
+  simple_problems = ["Sphere", "Schwefel2.22", "Schwefel2.22"]
+  for(problem in simple_problems)
+    p = GlobalOptim.Problems.examples[problem]
+    @fact de_fitness_for(p, 5, 20,  2e3) < 0.01 => true
+    @fact de_fitness_for(p, 10, 20, 5e3) < 0.01 => true
+    @fact de_fitness_for(p, 30, 25, 2e4) < 0.01 => true
+  end
 
-facts("Optimize sphere in 10 dimensions with DE") do
-  p = GlobalOptim.Problems.examples["Sphere"]
-  @fact de_fitness_for(p, 10, 20, 5000) < 0.01 => true
-end
-
-facts("Optimize sphere in 30 dimensions with DE") do
-  p = GlobalOptim.Problems.examples["Sphere"]
-  @fact de_fitness_for(p, 30, 25, 10000) < 0.01 => true
-end
-
-facts("Optimize schwefel2.22 in 5 dimensions with DE") do
-  p = GlobalOptim.Problems.examples["Schwefel2.22"]
-  @fact de_fitness_for(p, 5, 20, 2000) < 0.01 => true
-end
-
-facts("Optimize schwefel2.22 in 10 dimensions with DE") do
-  p = GlobalOptim.Problems.examples["Schwefel2.22"]
-  @fact de_fitness_for(p, 10, 20, 5000) < 0.01 => true
+  harder_problems = ["Schwefel1.2"]
+  for(problem in harder_problems)
+    p = GlobalOptim.Problems.examples[problem]
+    @fact de_fitness_for(p, 5, 20,  5e3) < 0.01 => true
+    @fact de_fitness_for(p, 10, 20, 2e4) < 0.01 => true
+    # Have to investigate why DE so bad for this one:
+    @fact de_fitness_for(p, 30, 20, 1e5) < 100.0 => true
+  end
 end
