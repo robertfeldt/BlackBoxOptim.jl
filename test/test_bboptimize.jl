499310cadb59a5e2e58e5d24f9650a30d742c999
Robert Feldt <robert.feldt@gmail.com>
2013-10-28 19:00:40 +0100

    added the other examples from the README to test_bboptimize

--- test/test_bboptimize.jl
+++ test/test_bboptimize.jl
@@ -3,6 +3,18 @@ function rosenbrock2d(x)
 end
 
 facts("bboptimize") do
-  best, fitness = bboptimize(rosenbrock2d, (-5.0, 5.0); dimensions = 2)
-  @fact fitness < 0.01 => true
+  context("example 1 from README") do
+    best, fitness = bboptimize(rosenbrock2d, (-5.0, 5.0); dimensions = 2)
+    @fact fitness < 0.001 => true
+  end
+
+  context("example 2 from README") do
+    best, fitness = bboptimize(rosenbrock2d, [(-5.0, 5.0), (0.0, 1.0)])
+    @fact fitness < 0.001 => true
+  end
+
+  context("example 3 from README") do
+    best, fitness = bboptimize(rosenbrock2d, (-5.0, 5.0); dimensions = 2, method = :de_rand_1_bin)
+    @fact fitness < 0.001 => true
+  end
 end
