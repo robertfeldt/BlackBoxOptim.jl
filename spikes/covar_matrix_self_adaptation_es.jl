517ca268cdea2f53e1ca38e22ace6715b5c70bca
Robert Feldt <robert.feldt@gmail.com>
2013-12-16 22:33:17 +0100

    moved problems so they are loaded on all machines

--- spikes/covar_matrix_self_adaptation_es.jl
+++ spikes/covar_matrix_self_adaptation_es.jl
@@ -266,6 +266,23 @@ function assign_weights(n, fitnesses, utilities; minimize = true)
   us_ordered
 end
 
+function rosenbrock(x)
+  n = length(x)
+  sum( 100*( x[2:n] - x[1:(n-1)].^2 ).^2 + ( x[1:(n-1)] - 1 ).^2 )
+end
+
+function sphere(x)
+  sum(x .^ 2)
+end
+
+function cigar(x)
+  x[1]^2 + 1e6 * sum(x[2:end].^2)
+end
+
+function cigtab(x)
+  x[1]^2 + 1e8 * x[end]^2 + 1e4 * sum(x[2:(end-1)].^2)
+end
+
 #function rosenbrock(x)
 #  n = length(x)
 #  sum( 100*( x[2:n] - x[1:(n-1)].^2 ).^2 + ( x[1:(n-1)] - 1 ).^2 )
