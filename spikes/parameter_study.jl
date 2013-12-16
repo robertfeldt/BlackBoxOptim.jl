72cfe735962da9bfd28e59bbc4f0b047c3a8f2b8
Robert Feldt <robert.feldt@gmail.com>
2013-12-16 22:31:21 +0100

    prep for param study

--- spikes/parameter_study.jl
+++ spikes/parameter_study.jl
@@ -4,51 +4,38 @@ require("covar_matrix_self_adaptation_es.jl")
 @everywhere cmsa_es(2, rosenbrock; max_fevals = 2, trace = false) 
 @everywhere cmsa_es(2, rosenbrock; max_fevals = 2, covarMatrixSampler = CholeskyCovarSampler, trace = false) 
 
-function rosenbrock(x)
-  n = length(x)
-  sum( 100*( x[2:n] - x[1:(n-1)].^2 ).^2 + ( x[1:(n-1)] - 1 ).^2 )
-end
-
-function sphere(x)
-  sum(x .^ 2)
-end
-
-function cigar(x)
-  x[1]^2 + 1e6 * sum(x[2:end].^2)
-end
-
-function cigtab(x)
-  x[1]^2 + 1e8 * x[end]^2 + 1e4 * sum(x[2:(end-1)].^2)
-end
-
 # Parameter study
-num_runs_per_dim = 1
-dims = 2.^(1:1)
+num_runs_per_dim = 25
+dims = 2.^(1:6)
 # Max evals per dim
-#max_evals_per_dim = [1000, 10000, 100000]
-max_evals_per_dim = [1000]
+max_evals_per_dim = [1000, 10000, 100000]
+#max_evals_per_dim = [1000]
 samplers = [EigenCovarSampler, CholeskyCovarSampler]
-#utils = [linear_utilities, log_utilities]
-utils = [linear_utilities]
+utils = [linear_utilities, log_utilities]
+#utils = [linear_utilities]
 #mus = [0.05, 0.10, 0.20, 0.30]
-#mus = [1, 0.05, 0.10, 0.20]
-mus = [1]
+mus = [1, 0.05, 0.10, 0.20]
+#mus = [1]
 lambdas = [(n) -> int(4 + 3*floor(log(n))), (n) -> 4*n, (n) -> 4*n*n]
-#problems = [rosenbrock, sphere, cigar, cigtab]
-problems = [rosenbrock]
+problems = [rosenbrock, sphere, cigar, cigtab]
+#problems = [rosenbrock]
+
+@everywhere machine1 = "macbook_pro_retina_cth"
+@everywhere machine2 = "macbook_pro_ssd_bth"
+@everywhere machine = machine2
 
 # Now create parameter combinations
 Params = Any[]
 for(run in 1:num_runs_per_dim)
   for(prob in problems)
     for(d in dims)
-      for(maxfevals in max_evals)
+      for(maxfevals in max_evals_per_dim)
         for(uf in utils)
           for(s in samplers)
             for(l in lambdas)
-              
+
               if typeof(l) == Function
-                l = l(n)
+                l = l(d)
               end
 
               for(mu in mus)
@@ -73,27 +60,30 @@ end
 
 @everywhere function one_run(args)
   run, prob, d, maxfevals, uf, s, mu, l = args
+
+  ts = strftime("%y%m%d,%H%M%S", time())
+
   tic()
   x, f, fevals = cmsa_es(d, prob; 
     max_fevals = maxfevals, covarMatrixSampler = s,
     mu = mu, lambda = l,
     utilitiesFunc = uf, trace = false)
-  time = toq()
+  exectime = toq()
 
-  return "$(run),$(prob),$(d),$(s),$(uf),$(mu),$(l),$(fevals),$(time),$(f)"
+  return "$(ts),$(run),$(prob),$(d),$(machine),$(s),$(uf),$(mu),$(l),$(fevals),$(exectime),$(f)"
 end
 
 @everywhere function one_run_and_save(args)
   res = one_run(args)
-  fh = open(strftime("%y%m%d_%H%M%S_$(args[2])_$(myid()).csv", time()), "w")
-  println(fh, res)
-  close(fh)
+  println(STDERR, res)
   res
 end
 
-results = pmap(one_run_and_save, Params[1:2])
+results = pmap(one_run_and_save, Params)
 
-println("\n\nRunID,N,Sampler,Utilities,Mu,Lambda,NumFuncEvals,Time,Fitness")
+fh = open(strftime("parameter_studies/%y%m%d_%H%M%S_$(machine).csv", time()), "w")
+println(fh, "Date,Time,RunID,Problem,N,Machine,Sampler,Utilities,Mu,Lambda,NumFuncEvals,ExecTime,Fitness")
 for(i in 1:length(results))
-  println(results[i])
+  println(fh, results[i])
 end
+close(fh)
