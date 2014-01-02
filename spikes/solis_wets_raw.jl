8239233d2e8044ae56e96cdcf2f00025d2d2b5c5
Robert Feldt <robert.feldt@gmail.com>
2014-01-02 01:32:58 +0100

    merged solis wets code into one

--- spikes/solis_wets_raw.jl
+++ spikes/solis_wets_raw.jl
@@ -1,9 +1,10 @@
-using Distributions
-using BlackBoxOptim
-
-function solis_wets(problem; max_evals_per_dim = 1e4, ftol = 1e-8,
+function solis_wets(problem; 
+  max_evals_per_dim = 1e5, ftol = 1e-7,
   max_success_steps = 5, max_fail_steps = 3,
-  min_improvement_potential = 1e-8, rnggen = (sigma) -> Normal(0, sigma))
+  rnggen = (sigma) -> Normal(0, sigma))
+
+  start_time = time()
+
   n = numdims(problem)
   ss = search_space(problem)
   num_successes = num_failures = 0
@@ -13,16 +14,12 @@ function solis_wets(problem; max_evals_per_dim = 1e4, ftol = 1e-8,
   max_evals = max_evals_per_dim * n
 
   xbest = rand_individual(ss)
-
   termination_reason = "unknown"
 
   a = BlackBoxOptim.TopListArchive(n, 10)
-  fbest = eval1(xbest, p)
-  add_candidate!(a, fbest, xbest[:])
+  fbest = eval1(xbest, problem)
   num_fevals = 1
-
-  # We map each barrier breach log10 value to its (num_fevals, fitness, width, fip)
-  barrier_breaches = Dict{Int64, (Int64, Float64, Float64, Float64)}()
+  add_candidate!(a, fbest, xbest[:], num_fevals)
 
   while(true)
     # Terminate if fitness within ftol of known fmin
@@ -31,21 +28,6 @@ function solis_wets(problem; max_evals_per_dim = 1e4, ftol = 1e-8,
       break
     end
 
-    # Save info about the fitness improvement potential each time a new log10
-    # ftol barrier is breached.
-    barrier_class = int(ceil(log10(fbest)))
-    if !haskey(barrier_breaches, barrier_class)
-      w = width_of_confidence_interval(a, 0.01)
-      fip = fitness_improvement_potential(a, 0.01)
-      barrier_breaches[barrier_class] = (num_fevals, fbest, w, fip)
-    end
-
-    # Terminate if improvement potential is less than 0.01% at p-value 0.01
-    #if fitness_improvement_potential(a, 0.01) < min_improvement_potential
-    #  termination_reason = "Not enough improvement potential"
-    #  break
-    #end
-
     if num_fevals > max_evals
       termination_reason = "Max evals budget exceeded"
       break
@@ -53,34 +35,34 @@ function solis_wets(problem; max_evals_per_dim = 1e4, ftol = 1e-8,
 
     diff = rand(rng, n)
     xplus = xbest + bias + diff
-    fxplus = eval1(xplus, p)
+    fxplus = eval1(xplus, problem)
     num_fevals += 1
 
     if fxplus < fbest
 
       fbest = fxplus
-      add_candidate!(a, fxplus, xplus[:])
+      add_candidate!(a, fxplus, xplus[:], num_fevals)
       num_successes += 1
       num_failures = 0
       bias = 0.2 * bias + 0.4 * (diff + bias)
       xbest = xplus
-      println( "$(num_fevals): New best, fitness = $(fbest), fip = $(fitness_improvement_potential(a, 0.01)), width = $(width_of_confidence_interval(a, 0.01))")
+      println( "$(num_fevals): New best, fitness = $(fbest), width = $(width_of_confidence_interval(a, 0.01))")
 
     else
 
       xminus = xbest - bias - diff
-      fxminus = eval1(xminus, p)
+      fxminus = eval1(xminus, problem)
       num_fevals += 1
 
       if fxminus < fbest
 
         fbest = fxminus
-        add_candidate!(a, fxminus, xminus[:])
+        add_candidate!(a, fxminus, xminus[:], num_fevals)
         num_successes += 1
         num_failures = 0
         bias = bias - 0.4 * (diff + bias)
         xbest = xminus
-        println( "$(num_fevals): New best, fitness = $(fbest), fip = $(fitness_improvement_potential(a, 0.01)), width = $(width_of_confidence_interval(a, 0.01))")
+        println( "$(num_fevals): New best, fitness = $(fbest), width = $(width_of_confidence_interval(a, 0.01))")
 
       else
         bias = bias / 2
@@ -95,26 +77,105 @@ function solis_wets(problem; max_evals_per_dim = 1e4, ftol = 1e-8,
       rng = rnggen(sigma)
       num_successes = 0
     elseif num_failures > max_fail_steps
-      sigma = max(1e-7, sigma / 2)
+      sigma = max(1e-20, sigma / 2)
       rng = rnggen(sigma)
       num_failures = 0
     end
 
   end
 
-  return xbest, best_fitness(a), num_fevals, termination_reason, a, barrier_breaches
+  # Save fitness history to a csv file
+  csvfile = strftime("solis_wets_%Y%m%d_%H%M%S.csv", start_time)
+  save_fitness_history_to_csv_file(a, csvfile)
+  println("Saved fitness history to file: $(csvfile)")
+
+  return xbest, best_fitness(a), num_fevals, termination_reason, a
 end
 
-gaussianrng = (sigma) -> Normal(0, sigma)
-levyrng = (sigma) -> Levy(0, sigma)
-
-p = as_fixed_dim_problem(BlackBoxOptim.example_problems["Rosenbrock"], 2^6)
-problem = BlackBoxOptim.shifted(p)
-xb, fb, nf, r, a, bbs = solis_wets(problem; min_improvement_potential = 1e-8, 
-  max_fail_steps = 3, max_evals_per_dim = 1e6, rnggen = gaussianrng)
-
-println("reason = $(r), fitness = $(fb)")
-bkeys = sort(collect(keys(bbs)), rev = true)
-for(k in bkeys)
-  println("$(k): $(bbs[k])")
-end
+#type SolisWetsOptimizer <: Optimizer
+#  # Current population: 1st column is current solution, 2nd columnn is current
+#  # candidate to consider. 
+#  pop::Array{Float64, 2}
+#  N::Int64      
+#  bias::Array{Float64, 1}   # Bias
+#  diff::Array{Float64, 1}   # Latest Diff vector.
+#  sigma::Float64            # Measure of spread in random sampling. Note! Sigma used in stead of rho=sigma^2
+#  rng::ContinuousMultivariateDistribution
+#  ss::SearchSpace
+#  num_successes::Int64
+#  num_failures::Int64
+#  state_is_plus::Bool       # Indicates which state we are in, i.e. if next candidate to generate is the plus or the minus candidate.
+#
+#  SolisWetsOptimizer(ss::SearchSpace, x = false; bias = false, sigma = false, rng = false) = begin
+#    if rng == false
+#      sigma = sigma ? sigma : (0.5*mean(diameters(ss)))
+#      rng = Normal(0, sigma)
+#    end
+#
+#    N = numdims(ss)
+#
+#    pop = zeros(N, 2)
+#    if x != false
+#      pop[:, 1] = x
+#    else
+#      pop[:, 1] = rand(rng, N)
+#    end
+#
+#    bias = zeros(N)
+#    diff = zeros(N)
+#
+#    new(pop, N, bias, bdiff, sigma, rng, ss, 0, 0, true)
+#  end
+#end
+#
+## Get a new set of candidate solutions.
+#function ask(sw::SolisWetsOptimizer)
+#  if sw.state_is_plus
+#    sw.diff = rand(sw.rng, sw.N)
+#    xdiff = sw.bias + sw.diff
+#  else
+#    xdiff = -sw.bias - sw.diff
+#  end
+#
+#  sw.pop[:, 2] = sw.pop[:, 1] + xdiff
+#
+#  return sw.pop, [2], [1]
+#end
+#
+#function increase_sigma(sw::SolisWetsOptimizer)
+#  sw.sigma = sw.sigma * 2
+#  sw.rng = Normal(0, sw.sigma)
+#end
+#
+#function decrease_sigma(sw::SolisWetsOptimizer)
+#  sw.sigma = sw.sigma / 2
+#  sw.rng = Normal(0, sw.sigma)
+#end
+#
+#function tell(sw::SolisWetsOptimizer, pop, ranks)
+#  success = ranks[1] == 2  # Success if the 2nd individual is ranked first.
+#
+#  if success
+#    sw.pop[:, 1] = sw.pop[:, 2]
+#    sw.num_successes += 1
+#    sw.num_failures = 0
+#    sign = sw.state_is_plus ? (+1) : (-1)
+#    sw.bias = 0.2 * sw.bias + sign * 0.4 * (sw.diff + sw.bias)
+#    # With a success we always go next in the plus direction.
+#    sw.state_is_plus = true
+#  else
+#    if sw.state_is_plus == false
+#      sw.num_successes = 0
+#      sw.num_failures += 1
+#      sw.state_is_plus = true
+#    end
+#  end
+#
+#  if sw.num_successes > 5
+#    increase_sigma(sw)
+#    sw.num_successes = 0
+#  elseif sw.num_failures > 3
+#    decrease_sigma(sw)
+#    sw.num_failures = 0
+#  end
+#end
