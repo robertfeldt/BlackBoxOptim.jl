07fbb7377a96c13aefa4a988d71d640cd2ff0c9a
Robert Feldt <robert.feldt@gmail.com>
2014-01-05 23:01:09 +0100

    extensions and bug fixes to better support parameter experiments and surrogate modeling

--- R/parameter_experiment.R
+++ R/parameter_experiment.R
@@ -4,20 +4,22 @@
 library(tgp)
 library(rjson)
 library(lhs)
+library(optparse, quietly = TRUE, warn.conflicts = FALSE, verbose = FALSE)
 
 
 #####################################################################
 # 0. Define constants and functions
 #####################################################################
 num_tgp_runs <- 4                 # Number of runs when building tgp models
-improvedlhs_num_repeats <- 100    # Number of points to consider for each added design point with improvedLHS
-lhs_sample_size <- 500            # Number of candidate points sampled when selecting design points
+improvedlhs_num_repeats <- 10     # Number of points to consider for each added design point with improvedLHS
+lhs_sample_size <- 200            # Number of candidate points sampled when selecting design points
 num_selected_design_points <- 30  # Number of selected design points for prediction
 
-sample_param_space <- function(ranges, num_samples) {
-  delta <- ranges[,2] - ranges[,1]
-  s <- improvedLHS(nrow(ranges), num_samples, improvedlhs_num_repeats)
-  t(ranges[,1] + delta * s)
+sample_param_space <- function(num_params, num_samples, repeats = improvedlhs_num_repeats) {
+  #delta <- ranges[,2] - ranges[,1]
+  s <- improvedLHS(num_samples, num_params, repeats)
+  #t(ranges[,1] + delta * s)
+  s
 }
 
 save_matrix_to_json_file <- function(matrix, filepath) {
@@ -33,27 +35,47 @@ save_matrix_to_json_file <- function(matrix, filepath) {
 
 args <- commandArgs(trailingOnly = TRUE)
 
-# Example args: 6 "c(0.0,3.0,0.0,3.0,0.0,2.0,0.0,2.0,0.6,1.0,-2.0,0.0)" 7 13 in.csv out.json
+# Example 
+# args <- c("6", "1", "15", "4", "cmsa_es_exp2.csv", "new_runs.json", "sa")
+# args <- c("4", "1", "10", "4", "cmsa_es_exp3.csv", "new_runs.json", "sa")
+# setwd("/Users/feldt/dev/BlackBoxOptim.jl/spikes/experiments")
 
 num_params <- as.integer(args[1]);
-ranges <- matrix(eval(parse(text = args[2])), byrow=TRUE, nrow=num_params);
-num_new_runs <- as.integer(args[3]);
-response_column <- as.integer(args[4]); # Response that should be minimized
+#ranges <- matrix(eval(parse(text = args[2])), byrow=TRUE, nrow=num_params);
+num_new_runs <- as.integer(args[2]);
+
+response_column <- as.integer(args[3]); # Response that should be minimized
+if(response_column < 0) {
+  invert_response <- TRUE;
+  response_column <- -response_column;
+} else {
+  invert_response <- FALSE;
+}
+
+basemax <- as.integer(args[4]); # basemax value, params larger than this are binary/categorical
 csvfile <- args[5];
 outfile <- args[6];
 
+if(length(args) >= 7) {
+  perform_sa <- args[7]
+} else {
+  perform_sa <- "not"
+}
+
+if(length(args) >= 8) {
+  selection_scheme <- tolower(args[8])
+} else {
+  selection_scheme <- "ei"
+}
+
 
 #####################################################################
-# 2. Read input csv
+# 2. Read input if the file exist
 #####################################################################
-runs <- read.csv(csvfile, header = TRUE)
-
-
-#####################################################################
-# 3. Build tgp model if there are any existing runs, if not we
-#    generate initial points via lhs. Save points to out file.
-#####################################################################
-if(nrow(runs) > 0) {
+csv_exists <- file.exists(csvfile)
+if(csv_exists) {
+  # Read input csv
+  runs <- read.csv(csvfile, header = TRUE)
 
   num_runs <- nrow(runs)
 
@@ -64,29 +86,125 @@ if(nrow(runs) > 0) {
   # Extract the Z response vector.
   Z = as.matrix(runs[,response_column])
 
+  # For using EI improv below we need to minimize. The sign of the response 
+  # column above indicated if we need to invert.
+  if(invert_response) {
+    Z = -Z
+  }
+
+}
+
+
+#####################################################################
+# 3. Perform sensitivity analysis if requested
+#####################################################################
+if(csv_exists && (tolower(perform_sa) == "sa" || tolower(perform_sa) == "true")) {
+
+  # Set general rect, mode and shape params
+  #rect <- t(apply(X, 2, range, na.rm=TRUE))
+  rect <- matrix(rep(0.0, 2*num_params), nrow=num_params)
+  rect[,2] <- 1.0
+  #mode <- apply(X , 2, mean, na.rm=TRUE)
+  #mode <- rep(0.5, num_params)
+  shape = rep.int(2, num_params)
+
+  # Update rect and shape for the categorical/binary vars.
+  #shape[(basemax+1):num_params] <- 0
+  #rect[(basemax+1):num_params,1] <- 0
+  #rect[(basemax+1):num_params,2] <- 1
+  #mode[(basemax+1):num_params] <- 0.5
+
+  cat("Performing sensitivity analysis\n")
+  s <- suppressWarnings(sens(X=X, Z=Z, nn.lhs=100, model=btgp, 
+#    shape = shape, rect = rect,
+    ngrid=100, span=0.3, BTE=c(5000,10000,10)))
+
+  #png('main_effects_per_var.png', width = 1600, height = 1200)
+  pdf('main_effects_per_var.pdf')
+  plot(s, layout="sens", main="", maineff=t(1:num_params))
+  dev.off()
+
+  #png('sensitivty_per_var.png', width = 1600, height = 1200)
+  pdf('sensitivity_per_var.pdf')
+  plot(s, layout="sens", maineff=FALSE)
+  dev.off()
+}
+
+#####################################################################
+# 4. Build tgp model if there are any existing runs, if not we
+#    generate initial points via lhs. Save points to out file.
+#####################################################################
+if(csv_exists && nrow(runs) > 0) {
+
   # Build model for selecting new points
-  model <- btgp(X=X, Z=Z, pred.n=FALSE, R=num_tgp_runs)
+  cat("Building model for selecting points\n")
+  model <- btgp(X=X, Z=Z, pred.n=FALSE, basemax = basemax, R=num_tgp_runs)
+  #model2 <- btgpllm(X=X, Z=Z, pred.n=FALSE, basemax = basemax, R=num_tgp_runs)
 
   # And select the best points from a LHS sample.
-  Xcandidates = sample_param_space(ranges, lhs_sample_size);
+  cat("Sampling many points to select from\n")
+  Xcandidates = sample_param_space(num_params, lhs_sample_size);
+  cat("Select a subset of points with most design value\n")
   num_points <- max(num_selected_design_points, num_new_runs);
   XX <- tgp.design(num_points, Xcandidates, model);
 
   # Now predict in those points
-  pmodel <- btgp(X=X, Z=Z, XX=XX, corr="exp", improv=TRUE,
-    Ds2x=TRUE, R=5, verb=0, krige = FALSE)
+  pmodel <- btgp(X=X, Z=Z, XX=XX, basemax = basemax, corr="exp", improv=TRUE,
+    R=num_tgp_runs, krige = FALSE)
+  #pmodel2 <- btgpllm(X=X, Z=Z, XX=XX, basemax = basemax, corr="exp", improv=TRUE,
+  #  Ds2x=TRUE, R=num_tgp_runs, krige = FALSE)
+
+  # Write the tgp tree to file
+  pdf('tgp_tree.pdf')
+  tgp.trees(pmodel)
+  dev.off()
+
+  # Write the posterior predictive surface per parameter to disk.
+  for(i in 1:num_params) {
+    name <- names(runs)[i+num_params]
+    pdf(paste('posterior_', i, '_', name, '.pdf', sep=""))
+    plot(pmodel, main=name, proj=c(i))
+    dev.off()
+  }
 
   # Predicted values
-  ZZ = m1p$ZZ.mean
+  ZZ = pmodel$ZZ.mean
+  #ZZ2 = pmodel2$ZZ.mean
 
-  # Find the num_new_points points with minimum values and select those points.
-  mins = sort(ZZ)
-  indmins = which(ZZ %in% mins[1:num_new_runs])
-  XXsel <- XX[indmins,]
+  if(selection_scheme == "min") {
+
+    cat("Selection scheme: minimization of response\n")
+
+    # Find the num_new_points points with minimum values and select those points.
+    mins = sort(ZZ)
+    index = which(ZZ %in% mins[1:num_new_runs])
+    #mins2 = sort(ZZ2)
+    #indmins2 = which(ZZ2 %in% mins2[1:num_new_runs])
+    XXsel <- XX[index,]
+
+  } else if(selection_scheme == "alc") {
+
+    cat("Selection scheme: Active Learning Cohn")
+
+
+  } else {
+
+    # Default scheme is "EI", i.e. minimization according to expected improvement
+    cat("Selection scheme: Expected Improvement\n")
+
+    # Find the first ranked design point according to EI
+    index <- which(pmodel$improv$rank <= num_new_runs)
+    XXsel <- XX[index[1:num_new_runs], ]
+
+  }
+
+  # Print some info
+  cat("XXsel = ", XXsel, "\n");
+  cat("Predicted values for XXsel, ZZ = ", ZZ[index], "\n");
 
 } else {
 
-  XXsel = sample_param_space(ranges, num_params+1);
+  XXsel = sample_param_space(num_params, num_params+1);
 
 }
 
--- spikes/experiments/cmsa_es_tgp.jl
+++ spikes/experiments/cmsa_es_tgp.jl
@@ -5,8 +5,8 @@ require("../covar_matrix_self_adaptation_es.jl")
 require("../experiment_framework.jl")
 
 type ParameterExperiment
-  parameters::Array{ASCIIString, 1}           # Parameter names
-  mappers::Array{Function, 1}                 # One function per param mapping a design value to a param value
+  parameters::Array{ASCIIString, 1}             # Parameter names
+  mappers::Array{Function, 1}                   # One function per param mapping a design value to a param value
   design_ranges::Array{(Float64, Float64), 1}   # Design range per parameter
 end
 
@@ -15,22 +15,24 @@ p = as_fixed_dim_problem(BlackBoxOptim.example_problems["Rosenbrock"], n)
 p = BlackBoxOptim.shifted(p)
 diameter = minimum(diameters(search_space(p)))
 
-cmsa_es_exp1 = ParameterExperiment(
-  ["lambda", "mu", "covarMatrixSampler", "utilitiesFunc", 
-   "covar_learning_rate", "sigma"],
-  [((ds, ps) -> 4 * [2, n, n*n][int(ceil(ds[1]))]),
-   ((ds, ps) -> int(ps[1] / [2, 4, 8][int(ceil(ds[2]))])),
-   ((ds, ps) -> [EigenCovarSampler, CholeskyCovarSampler][int(ceil(ds[3]))]),
-   ((ds, ps) -> [log_utilities, linear_utilities][int(ceil(ds[4]))]),
-   ((ds, ps) -> ds[5]),
-   ((ds, ps) -> diameter * 10^ds[6])
+pe = cmsa_es_exp2 = ParameterExperiment(
+  ["covar_learning_rate", "sigma", "lambda", "mu", 
+    "covarMatrixSampler", "utilitiesFunc"
   ],
-  [( 0.0, 3.0),
-   ( 0.0, 3.0),
-   ( 0.0, 2.0),
-   ( 0.0, 2.0),
-   ( 0.6, 1.0),
-   (-2.0, 0.0)]
+  [((ds, ps) -> ds[1]),
+   ((ds, ps) -> diameter * 10^ds[2]),
+   ((ds, ps) -> int(round(ds[3]))),
+   ((ds, ps) -> int(max(1.0, ceil(ps[3] / ds[4])))),
+   ((ds, ps) -> ds[5] < 0.5 ? EigenCovarSampler : CholeskyCovarSampler),
+   ((ds, ps) -> ds[6] < 0.5 ? linear_utilities : log_utilities)
+  ],
+  [( 0.6, 1.0),
+   (-2.0, 0.0),
+   ( 8.0, float(n*n)),
+   ( 2.0, 8.0),
+   ( 0.0, 1.0),
+   ( 0.0, 1.0)
+  ]
 )
 
 # Return true iff a param with a given index is fixed, i.e. if its range
@@ -45,26 +47,26 @@ num_fixed_params(pe::ParameterExperiment) = numparams(pe) - num_varying_params(p
 index_to_varying_params(pe::ParameterExperiment) = filter( (i) -> !is_fixed_param(pe, i), 1:numparams(pe) )
 value_for_fixed_param(pe::ParameterExperiment, i) = pe.design_ranges[i][1]
 
-function design_ranges_to_R_array(pe::ParameterExperiment)
-  ary = Float64[]
-  for(i in 1:numparams(pe))
-    if !is_fixed_param(pe, i)
-      push!(ary, float(pe.design_ranges[i][1]))
-      push!(ary, float(pe.design_ranges[i][2]))
-    end
-  end
-  array_to_R_string(ary)
-end
-
-function array_to_R_string(a)
-  join(["c(", join(a, ","), ")"])
-end
+#function design_ranges_to_R_array(pe::ParameterExperiment)
+#  ary = Float64[]
+#  for(i in 1:numparams(pe))
+#    if !is_fixed_param(pe, i)
+#      push!(ary, float(pe.design_ranges[i][1]))
+#      push!(ary, float(pe.design_ranges[i][2]))
+#    end
+#  end
+#  array_to_R_string(ary)
+#end
+#
+#function array_to_R_string(a)
+#  join(["c(", join(a, ","), ")"])
+#end
 
 function write_csv_header_to_file(pe::ParameterExperiment, filepath)
   header = join([
     map((i) -> "d$(i)", 1:numparams(pe)), 
     pe.parameters,
-    ["Fitness", "NumFuncEvals", "Time", "Reason"]],
+    ["MedianFitness", "MedianNumFuncEvals", "SuccessRate"]],
     ",")
   fh = open(filepath, "w")
   println(fh, header)
@@ -72,7 +74,7 @@ function write_csv_header_to_file(pe::ParameterExperiment, filepath)
 end
 
 # Now we want to invoke an R script like so:
-# Rscript top_of_bbo_path/R/parameter_experiment.R 6 "c(0.0,3.0,0.0,3.0,0.0,2.0,0.0,2.0,0.6,1.0,-2.0,0.0)" 7 13 in.csv out.json
+# Rscript top_of_bbo_path/R/parameter_experiment.R 6 7 13 in.csv out.json
 # with parameters indicating: 6 = num params, ranges, num runs, response col, in.csv, out.json
 # where in.csv has the values used so far and out.json has the 7*6 new design values.
 
@@ -83,8 +85,12 @@ function read_matrix_from_file(filename, nrows, ncols)
   reshape(fromR, nrows, ncols)
 end
 
+function map01_range_to_design_range(value01, design_min, design_max)
+  design_min + value01 * (design_max - design_min)
+end
+
 function create_design_matrix_from_file_and_fixed_params(pe::ParameterExperiment, filename, num_rows)
-  design = read_matrix_from_file(filename, num_rows, num_varying_params(pe))
+  design01 = read_matrix_from_file(filename, num_rows, num_varying_params(pe))
 
   ds = zeros(num_rows, numparams(pe))
   next_design_col = 1
@@ -93,15 +99,26 @@ function create_design_matrix_from_file_and_fixed_params(pe::ParameterExperiment
     if is_fixed_param(pe, i)
       ds[:, i] = value_for_fixed_param(pe, i) * ones(num_rows)
     else
-      ds[:, i] = design[:,next_design_col]
+      dmin, dmax = pe.design_ranges[i]
+      ds[:, i] = map01_range_to_design_range(design01[:,next_design_col], dmin, dmax)
       next_design_col += 1
     end
   end
 
-  ds
+  return design01, ds
 end
 
-function run_based_on_design_matrix_while_saving_to_csvfile(pe::ParameterExperiment, design, outfilepath)
+function run_based_on_design_matrix_in_file_while_saving_results_to_csvfile(pe::ParameterExperiment, outfilepath, num_rows; 
+  designfile = "new_runs.json", num_repeats = 10, 
+  fixed_params = {:max_evals_per_dim => 1e7, 
+    :covarMatrixSampler => CholeskyCovarSampler,
+    :utilitiesFunc => log_utilities})
+
+  if !isfile(outfilepath)
+    write_csv_header_to_file(pe, outfilepath)
+  end
+
+  design01, design = create_design_matrix_from_file_and_fixed_params(pe, designfile, num_rows)
 
   for(i in 1:size(design,1))
     ds = design[i,:]
@@ -110,24 +127,137 @@ function run_based_on_design_matrix_while_saving_to_csvfile(pe::ParameterExperim
 
     # Calc the parameter values from the desing values.
     for(j in 1:numparams(pe))
-      param_dict[pe.parameters[j]] = pv = pe.mappers[j](ds, ps)
+      param_dict[symbol(pe.parameters[j])] = pv = pe.mappers[j](ds, ps)
       push!(ps, pv)
     end
 
     # Add other arguments.
-    param_dict[:max_evals_per_dim] = 1e7
+    param_dict = merge(param_dict, fixed_params)
+    print("params: "); show(param_dict); println("")
+
+    # Set up for saving results
+    fbs = zeros(num_repeats)
+    nfs = zeros(num_repeats)
+    num_within_ftol = 0
 
     # Now run it while timing.
-    tic()
-    xb, fb, nf, r, a = cmsa_es(p; collect(param_dict)...)
-    t = toq()
+    for(r in 1:num_repeats)
+      tic()
+      xb, fbs[r], nfs[r], r, a = cmsa_es(p; collect(param_dict)...)
+      et = toq()
+      if r == "Within ftol"
+        num_within_ftol += 1
+      end
+    end
 
     # Save info to the csv file
     fh = open(outfilepath, "a+")
-    print(fh, join(map( (dv) -> "$(dv)", ds), ","))
+    print(fh, join(map( (dv) -> "$(dv)", design01[i,:]), ","))
     print(fh, ",")
     print(fh, join(map( (pv) -> "$(pv)", ps), ","))
-    println(fh, ",", fb, ",", nf, ",", t, ",", r)
+    println(fh, ",", median(fbs), ",", median(nfs), ",", num_within_ftol/num_repeats)
+    close(fh)
   end
 
 end
+
+# It seems clear that (in order of sensitivity)
+#  1. covar_learning_rate needs to be high (highest sensitivity)
+#  2. mu should be a rather high divisor, at least 4, maybe even 8 
+#  3. we should use log_utilities (but large variation on this one so unclear)
+#  4. sigma should be rather low (but low sensitivity)
+#  5. lambda should be high, rather than low (low s)
+#  6. the covar sampler does not have a large effect but Cholesky seems somewhat better
+#  
+# We design a new experiment in line with this:
+
+pe3 = cmsa_es_exp3 = ParameterExperiment(
+  ["covar_learning_rate", "sigma", "lambda", "mu", 
+  ],
+  [((ds, ps) -> ds[1]),
+   ((ds, ps) -> diameter * 10^ds[2]),
+   ((ds, ps) -> int(round(ds[3]))),
+   ((ds, ps) -> int(max(1.0, ceil(ps[3] / ds[4]))))
+  ],
+  [( 0.75, 1.0),
+   (-4.0, -1.0),
+   (float(4*n), float(4*n*n)),
+   ( 3.0, 16.0)
+  ]
+)
+
+# There is quite a lot of variation for all of these covariates, except for
+# low values of covariate 1 but conclusions are:
+#   1. mu divisor should be larger than 5.6 (3+(16-3)*0.20) and higher values seem to be good
+#   2. lambda should be between 0.10-0.35 of the range 4*n to 4*n*n
+#   3. covar_learning_rate should be between 0.35-0.60 of the range 0.75-1.0 so in range 0.84-0.90
+#   4. sigma should be 0.80-1.0 in its range so 10^[-2, -1] of diameter with higher values giving quicker responses.
+
+# Based on these 2 experiments (exp2 and exp3) we conclude that:
+#   1. covar_learning_rate should be in 0.85-0.90
+#   2. lambda should be circa 4*n+.20*(4*n*n - 4*n)
+#   3. mu divisor should be in 5-16
+#   4. sigma should be 0.01-0.10 of the diameter
+# Lets now try these settings on the Rosenbrock with 16 dims.
+
+n = 16
+p = as_fixed_dim_problem(BlackBoxOptim.example_problems["Rosenbrock"], n)
+p = BlackBoxOptim.shifted(p)
+diameter = minimum(diameters(search_space(p)))
+
+pe4 = cmsa_es_exp4 = ParameterExperiment(
+  ["covar_learning_rate", "sigma", "lambda", "mu", 
+  ],
+  [((ds, ps) -> ds[1]),
+   ((ds, ps) -> diameter * 10^ds[2]),
+   ((ds, ps) -> int(round(ds[3]))),
+   ((ds, ps) -> int(max(1.0, ceil(ps[3] / ds[4]))))
+  ],
+  [( 0.84, 0.99),
+   (-2.0, -0.5),
+   (float(4*n), float(4*n*n)),
+   ( 5.0, 20.0)
+  ]
+)
+
+outfile = "cmsa_es_exp4.csv"
+write_csv_header_to_file(pe4, outfile)
+
+# Optimize to minimize the fail rate, i.e. inverted success rate:
+# /usr/bin/Rscript ../../R/parameter_experiment.R 4 5 -11 4 cmsa_es_exp4.csv new_runs.json
+
+run_based_on_design_matrix_in_file_while_saving_results_to_csvfile(pe4, outfile, 5; num_repeats = 5)
+
+# /usr/bin/Rscript ../../R/parameter_experiment.R 4 5 -11 4 cmsa_es_exp4.csv new_runs.json
+
+# For Rosenbrock 16 we saw good performance for the same param values as for
+# the 8-d version. In detail we conclude:
+#   1. best performance for lambda closer to 4*n than higher (highest sens)
+#   2. best performance for low mu all the way down to divisor 20 (high sens)
+#   3. covar learning rate should be from around 0.89-0.95 but worse if higher
+#   4. fairly insensitive to sigma as long as its around 0.05-0.20 of diameter (lowest sens)
+# 
+# Lets now try these settings on the Rosenbrock with 32 dims.
+
+n = 32
+p = as_fixed_dim_problem(BlackBoxOptim.example_problems["Rosenbrock"], n)
+p = BlackBoxOptim.shifted(p)
+diameter = minimum(diameters(search_space(p)))
+
+pe5 = ParameterExperiment(
+  ["covar_learning_rate", "sigma", "lambda", "mu", 
+  ],
+  [((ds, ps) -> ds[1]),
+   ((ds, ps) -> diameter * 10^ds[2]),
+   ((ds, ps) -> int(round(ds[3]))),
+   ((ds, ps) -> int(max(1.0, ceil(ps[3] / ds[4]))))
+  ],
+  [( 0.84, 0.99),
+   (-2.0, -0.5),
+   (float(2*n), float(2*n*n)),
+   ( 5.0, 25.0)
+  ]
+)
+
+outfile = "cmsa_es_exp5.csv"
+write_csv_header_to_file(pe5, outfile)
