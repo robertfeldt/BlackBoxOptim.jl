8210c8e33590fbdcf91c18cb64f0995b0dea8e7d
Robert Feldt <robert.feldt@gmail.com>
2014-02-17 10:01:46 +0100

    change trefethen example to use the new comparison and reporting functions

--- examples/trefethen_function.jl
+++ examples/trefethen_function.jl
@@ -2,10 +2,21 @@ using BlackBoxOptim
 
 # From Hans W. Borchers in 
 # https://groups.google.com/forum/#!topic/julia-opt/HltM-OGjueo
-function ftrefethen(p)
+function trefethen(p)
   x = p[1]; y = p[2]
   return exp(sin(50 * x)) + sin(60 * exp(y)) + sin(70 * sin(x)) + 
              sin(sin(80 * y)) - sin(10 * (x + y)) + (x^2 + y^2)/4
 end
 
-bboptimize(ftrefethen, (-1.0, 1.0); dimensions = 2)
+p = BlackBoxOptim.fixeddim_problem(trefethen; range = (-1.0, 1.0), dims = 2, name = "trefethen")
+
+# To just run one optimization:
+#bboptimize(p; max_time = 2.0, method = :generating_set_search)
+
+# To compare several methods in several repetitions:
+BlackBoxOptim.repeated_bboptimize(5, p, 2, [
+  :generating_set_search, 
+  :adaptive_de_rand_1_bin_radiuslimited,
+  :random_search,
+  :separable_nes], 
+  5.0)
