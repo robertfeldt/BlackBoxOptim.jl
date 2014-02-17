7173fe7698191f28e3784fee2f511c5214553ea4
Robert Feldt <robert.feldt@gmail.com>
2014-02-17 09:42:56 +0100

    updates to comparison test

--- test/test_compare_optimizers.jl
+++ test/test_compare_optimizers.jl
@@ -1,12 +1,12 @@
 facts("compare_optimizers") do
   context("comparing optimizers on more than one problem") do
-    #methods = MethodNames
-    methods = [:adaptive_de_rand_1_bin_radiuslimited, :separable_nes,
+    #methods = BlackBoxOptim.MethodNames
+    methods = [:adaptive_de_rand_1_bin_radiuslimited, :generating_set_search, :separable_nes, :xnes
       #:random_search,
       #:simultaneous_perturbation_stochastic_approximation
     ]
-    problems = BlackBoxOptim.as_fixed_dim_problem_set(BlackBoxOptim.example_problems, [5, 50])
-    ranks, fitnesses = BlackBoxOptim.compare_optimizers(problems; max_time = 10.0, methods = methods)
+    problems = BlackBoxOptim.as_fixed_dim_problem_set(BlackBoxOptim.example_problems, [50, 500])
+    ranks, fitnesses = BlackBoxOptim.compare_optimizers(problems; max_time = 60.0, methods = methods)
     @fact size(ranks, 2) => length(problems)
   end
 end
