815124a7ee1e92dea3276ec4cd3cc18567354768
Robert Feldt <robert.feldt@gmail.com>
2014-07-07 18:18:13 +0200

    added test to increase code coverage

---
+++ test/test_all_problems.jl
@@ -0,0 +1,12 @@
+facts("Problems") do
+
+  context("Can create fixed dimensional problem with search space as array") do
+    p = BlackBoxOptim.fixeddim_problem((x)->x; search_space = [(0.0, 1.0)])
+    @fact typeof(p) => BlackBoxOptim.FixedDimProblem
+  end
+
+  context("Exception if no valid search space supplied when creating fixed dimensional problem") do
+    @fact_throws BlackBoxOptim.fixeddim_problem((x)->x; search_space = (0.0, 1.0))
+  end
+
+end
