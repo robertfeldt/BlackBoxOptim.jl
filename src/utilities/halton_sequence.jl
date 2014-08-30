5dfcd5573db2d2e17c2afb513d4854ed42f3dd68
Robert Feldt <robert.feldt@gmail.com>
2014-08-30 11:57:56 +0200

    halton sequence generation (to be used for quasi-random restarts)

---
+++ src/utilities/halton_sequence.jl
@@ -0,0 +1,34 @@
+# Generate the first n numbers in Halton's sequence with base b.
+#
+#   seq = haltonsequence(b, n)
+#
+# Output:
+#  seq - sequence of n Halton numbers
+#
+# Inputs:
+#  b - base of the sequence
+#  n - length of vector to be generated
+#
+function haltonsequence(b, n)
+  map((i) -> haltonnumber(b, i), 1:n)
+end
+
+# Generate the nth Halton number in the sequence with base b.
+#
+# Notes:
+#  Implementation is based on the psudo code in:
+#    http://en.wikipedia.org/wiki/Halton_sequence
+#
+function haltonnumber(base, index)
+  res = 0
+  f = 1 / base
+  i = index
+
+  while (i > 0)
+    res += f * (i % base)
+    i = ifloor(i / base)
+    f = f / base
+  end
+
+  return res
+end
---
+++ test/test_halton_sequence.jl
@@ -0,0 +1,44 @@
+haltonnumber = BlackBoxOptim.Utils.haltonnumber
+haltonsequence = BlackBoxOptim.Utils.haltonsequence
+
+# The Halton sequence with base 2 according to http://en.wikipedia.org/wiki/Halton_sequence
+HaltonSequence2 = [
+  1/2, 1/4, 3/4, 1/8, 5/8, 3/8, 7/8, 1/16, 9/16
+]
+
+# The Halton sequence with base 2 according to http://en.wikipedia.org/wiki/Halton_sequence
+HaltonSequence3 = [
+  1/3, 2/3, 1/9, 4/9, 7/9, 2/9, 5/9, 8/9, 1/27
+]
+
+facts("Halton numbers and sequence") do
+
+  context("Halton numbers") do
+
+    map(1:length(HaltonSequence2)) do i
+      @fact isapprox(haltonnumber(2, i), HaltonSequence2[i]) => true
+    end
+ 
+    map(1:length(HaltonSequence3)) do i
+      @fact isapprox(haltonnumber(3, i), HaltonSequence3[i]) => true
+    end
+  
+  end
+
+  context("Halton sequences") do
+
+    hseq2 = haltonsequence(2, length(HaltonSequence2))
+    @fact length(hseq2) => length(HaltonSequence2)
+    map(1:length(HaltonSequence2)) do i
+      @fact isapprox(hseq2[i], HaltonSequence2[i]) => true
+    end
+
+    hseq3 = haltonsequence(3, length(HaltonSequence3))
+    @fact length(hseq3) => length(HaltonSequence3)
+    map(1:length(HaltonSequence3)) do i
+      @fact isapprox(hseq3[i], HaltonSequence3[i]) => true
+    end
+
+  end
+
+end
