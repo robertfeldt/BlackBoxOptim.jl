569c03602580ad076fd6d894ed82759db0c2941a
Robert Feldt <robert.feldt@gmail.com>
2014-09-21 16:42:43 +0200

    better nlopt testing

--- spikes/nlopt_highdim.jl
+++ spikes/nlopt_highdim.jl
@@ -1,41 +1,41 @@
 using NLopt
 using DataFrames
 
-function rosenbrock(x)
-  n = length(x)
-  return( sum( 100*( x[2:n] - x[1:(n-1)].^2 ).^2 + ( x[1:(n-1)] - 1 ).^2 ) )
-end
+include(joinpath("..", "src", "problems", "single_objective_base_functions.jl"))
 
-function rastrigin(x)
-  D = length(x)
-  10 * D + sum( x.^2 ) - 10 * sum( cos( 2 * π * x ) )
-end
+abstract Evaluator
 
-function ackley(x)
-  D = length(x)
-  try
-    20 - 20.*exp(-0.2.*sqrt(sum(x.^2)/D)) - exp(sum(cos(2 * π * x))/D) + e
-  catch e
-    # Sometimes we have gotten a DomainError from the cos function so we protect this call
-    println(e)
-    println("For input x = ", x)
-    # Return a very large fitness value to indicate that this is NOT the solution we want.
-    # TODO: Fix better way to handle this!
-    9.99e100
+type NonGradientEvaluator <: Evaluator
+  func::Function
+  fevals::Int
+  NonGradientEvaluator(func) = new(func, 0)
+end
+import Base.reset
+reset(e::Evaluator) = (e.fevals = 0)
+fevals(e::Evaluator) = e.fevals
+
+function makeobjfunc(e::Evaluator)
+  ofunc = (x::Vector, grad::Vector) -> begin
+    e.fevals::Int += 1
+    e.func(x)
   end
+  ofunc
 end
 
-numfevals = 0 # keep track of # function evaluations
+LocalNLoptAlgs = [:LN_COBYLA, :LN_BOBYQA, :LN_NEWUOA, :LN_PRAXIS, :LN_NELDERMEAD, :LN_SBPLX]
+GlobalNLoptAlgs = [:GN_DIRECT, :GN_DIRECT_L, :GN_ORIG_DIRECT, :GN_ORIG_DIRECT_L, 
+  :GN_CRS2_LM, :GN_ISRES, :GN_ESCH]
 
-function make_objfunc(funcToOptimize)
-  function objf(x::Vector, grad::Vector)
-    global numfevals
-    numfevals::Int += 1
-    funcToOptimize(x)
+# Randomly select np pairs of algs where the first is global and the second is local.
+function random_pairs_of_algs(np)
+  algs = Symbol[]
+  for i in 1:np
+    push!(algs, shuffle(GlobalNLoptAlgs)[1])
+    push!(algs, shuffle(LocalNLoptAlgs)[1])
   end
-  return objf
+  algs
 end
-
+    
 function compare_nlopt_algs(funcToOpt, dim;
   LowBound = -10.0,
   HighBound = 10.0,
@@ -89,36 +89,27 @@ function compare_nlopt_algs(funcToOpt, dim;
   res
 end
 
-r1 = compare_nlopt_algs(rosenbrock, 8; NumReps = 1, MaxTimes = [0.1, 1.0, 3.0])
-
-numfevals = 0
-
-function rosenbrock_objfunc(x::Vector, grad::Vector)
-    # We don't have gradient...
-
-  global numfevals
-  numfevals::Int += 1
-
-  rosenbrock(x)
-end
-
 # Given an array of NLopt algs and a vector of relative times to apply them
 # we run them in sequence on the value of the previous one.
-function combine_nlopt_algs(objfunc, dim;
+function combine_nlopt_algs(func, dim;
   Algs = nothing, RelTimes = nothing,
-  LowBound = -10.0,
-  HighBound = 10.0,
-  MaxTime = nothing
+  LowBound = -100.0,
+  HighBound = 100.0,
+  MaxTime = nothing,
+  ResetToBestProbability = 0.80,
+  FTOL_REL = -Inf,
+  FTOL_ABS = -Inf
   )
 
   if Algs == nothing
     numpairs = iceil(log10(dim))
-    Algs = vcat([:GN_DIRECT, :LN_PRAXIS], random_pairs_of_algs(numpairs), shuffle(LocalNLoptAlgs))
-    RelTimes = vcat(rand(2 + 2*numpairs), 0.05 * rand(length(LocalNLoptAlgs)))
+    Algs = vcat([:GN_DIRECT_L, :LN_PRAXIS], random_pairs_of_algs(numpairs), 
+      shuffle(LocalNLoptAlgs), [:LN_PRAXIS])
+    RelTimes = vcat([1, 3], rand(1:3, 2*numpairs), 0.30 * rand(length(LocalNLoptAlgs)), 2)
   end
 
   if RelTimes == nothing
-    rand(length(Algs))
+    RelTimes = rand(length(Algs))
   end
 
   if MaxTime == nothing
@@ -134,66 +125,137 @@ function combine_nlopt_algs(objfunc, dim;
   fmin = ret = 0
   global numfevals = 0
 
+  evaluator = NonGradientEvaluator(func)
+  objfunc = makeobjfunc(evaluator)
+
   xbest = x = randn(dim)
   fbest = objfunc(xbest, zeros(dim))
+  numtolreached = fevalsbest = 0
 
+  start_time = time()
+
+  println("\nStarting solve sequence...")
   for i in 1:numsteps
     opt = Opt(Algs[i], dim)
     lower_bounds!(opt, (LowBound  * ones(dim)))
     upper_bounds!(opt, (HighBound * ones(dim)))
     maxt = MaxTime * percenttimes[i]
     maxtime!(opt, maxt)
-    ftol_rel!(opt, 1e-7)
-    xtol_rel!(opt, 1e-7)
+    ftol_rel!(opt, FTOL_REL)
+    ftol_abs!(opt, FTOL_ABS)
     min_objective!(opt, objfunc)
-    (fmin, x, ret) = optimize(opt, x)
-    println(Algs[i], " for ", round(maxt, 3), " s, fmin = ", fmin, ", ret = ", ret)
+    try
+      tic()
+      (fmin, x, ret) = optimize(opt, x)
+      t = toq()
+      println(Algs[i], " for ", round(t, 3), " s (", round(t/maxt*100.0, 2), "%), fmin = ", fmin, ", ret = ", ret)
+    catch err
+      println(Algs[i], " failed. Skipping...")
+    end
+    nowtime = time()
     if fmin < fbest
       fbest = fmin
       xbest = x
+      fevalsbest = fevals(evaluator)
+      timebest = nowtime
     else
-      x = xbest
+      #if rand() < ResetToBestProbability
+      #  x = xbest
+      #end
+    end
+    elapsed = nowtime - start_time
+    if (nowtime - timebest) > 0.20 * MaxTime
+      if in(ret, [:SUCCESS, :FTOL_REACHED, :XTOL_REACHED, :ROUNDOFF_LIMITED])
+        if numtolreached < 1
+          # If first time to converge we try once more from a new random position
+          numtolreached += 1
+          x = randn(dim)
+          println("Restart since ret = ", ret)
+        else
+          println("Return since ret = ", ret, " and numtolreached = ", numtolreached)
+          return (fbest, xbest, :CONVERGED, fevalsbest, fevalsbest/dim, fevals(evaluator), fevals(evaluator)/dim, time() - start_time)
+        end
+      end
     end
   end
 
-  (fbest, xbest, ret, copy(numfevals))
+  return (fbest, xbest, ret, fevalsbest, fevalsbest/dim, fevals(evaluator), fevals(evaluator)/dim, time() - start_time)
 end
 
-LocalNLoptAlgs = [:LN_COBYLA, :LN_BOBYQA, :LN_NEWUOA, :LN_PRAXIS, :LN_NELDERMEAD, :LN_SBPLX]
-GlobalNLoptAlgs = [:GN_DIRECT, :GN_DIRECT_L, :GN_CRS2_LM, :GN_ISRES, :GN_ESCH]
+dfmapper(r) = begin
+  DataFrame(nfevalsbest = r[4], nfevals = r[6], nfbpd = r[5], fmin = r[1])
+end
 
-# Randomly select np pairs of algs where the first is global and the second is local.
-function random_pairs_of_algs(np)
-  algs = Symbol[]
-  for i in 1:np
-    push!(algs, shuffle(GlobalNLoptAlgs)[1])
-    push!(algs, shuffle(LocalNLoptAlgs)[1])
+SOPS = [sphere, ellipsoid, elliptic, cigar, cigtab, ackley, schwefel1_2, 
+  schwefel2_22, schwefel2_21, rastrigin, rosenbrock]
+
+SOPSNonzero = [step, griewank, schwefel2_26, shekel10]
+
+function xrotatedandshifted(n, f, shiftAmplitude = 1.0, rotateAmplitude = 1.0)
+  shift = shiftAmplitude * randn(n, 1)
+  rotmatrix = rotateAmplitude * rand(n, n)
+  transformed_f(x) = f(rotmatrix * (x .- shift))
+end
+
+#res8 = map((sop) -> combine_nlopt_algs(sop, 8), SOPS)
+#resrot8 = map((sop) -> combine_nlopt_algs(xrotatedandshifted(8, sop), 8), SOPS)
+
+#res64 = map((sop) -> combine_nlopt_algs(sop, 64), SOPS)
+#resrot64 = map((sop) -> combine_nlopt_algs(xrotatedandshifted(64, sop), 64), SOPS)
+
+#res256 = map((sop) -> combine_nlopt_algs(sop, 256), SOPS)
+#resrot256 = map((sop) -> combine_nlopt_algs(xrotatedandshifted(256, sop), 256), SOPS)
+
+function compare_optimizers(optimizers, problems, dim, numreps = 10;
+  dataframe_mapper = identity)
+
+  timestamp = strftime("%Y%m%d_%H%M%S", time())
+  filename = "results_nlopt_$(timestamp).csv"
+  res = DataFrame()
+  its = 0
+  for rep in 1:numreps
+    for (probname, origproblem) in problems
+      problem = xrotatedandshifted(dim, origproblem)
+      for (optname, opt) in optimizers
+        its += 1
+        println("Optimization ", its)
+        tic()
+        r = opt(problem, dim)
+        t = toq()
+        res = rbind(res, cbind(
+          DataFrame(Opt = optname, Prob = probname, Dim = dim, Time = round(t, 2)),
+          dataframe_mapper(r)
+          ))
+        writetable(filename, res)
+        println("Wrote results to file: ", filename)
+      end
+    end
   end
-  algs
+  return res, filename
 end
 
-numpairs = 4 
-rosenbrock_of = make_objfunc(rosenbrock)
-r = combine_nlopt_algs(rosenbrock_of, 64; MaxTime = 30.0, 
-  Algs = vcat([:LN_PRAXIS], random_pairs_of_algs(numpairs)), RelTimes = rand(1+2*numpairs))
+Problems = {
+  "Sphere      " => sphere,
+  "Rosenbrock  " => rosenbrock,
+  "Schwefel2_22" => schwefel2_22,
+  "Schwefel2_21" => schwefel2_21, 
+  "Rastrigin   " => rastrigin,
+}
 
-rastrigin_of = make_objfunc(rastrigin)
-numpairs = 4 
-r = combine_nlopt_algs(rastrigin_of, 8; MaxTime = 3.0, 
-  Algs = vcat([:LN_PRAXIS], random_pairs_of_algs(numpairs)), RelTimes = rand(1+2*numpairs))
+DP = [:GN_DIRECT_L, :LN_PRAXIS]
+DPN = [:GN_DIRECT_L, :LN_PRAXIS, :LN_NELDERMEAD]
 
-ackley_of = make_objfunc(ackley)
-numpairs = 4 
-r = combine_nlopt_algs(ackley_of, 8; MaxTime = 3.0, 
-  Algs = vcat([:GN_DIRECT, :LN_PRAXIS], random_pairs_of_algs(numpairs)), RelTimes = rand(2+2*numpairs))
-r = combine_nlopt_algs(rastrigin_of, 8; MaxTime = 3.0, 
-  Algs = vcat([:GN_DIRECT, :LN_PRAXIS], random_pairs_of_algs(numpairs)), RelTimes = rand(2+2*numpairs))
-r = combine_nlopt_algs(rosenbrock_of, 16; MaxTime = 3.0, 
-  Algs = vcat([:GN_DIRECT, :LN_PRAXIS], random_pairs_of_algs(numpairs)), RelTimes = rand(2+2*numpairs))
+Optimizers = {
+  "DirL1+Prax3       " => (pr, d) -> combine_nlopt_algs(pr, d; Algs = DP, RelTimes = [1,3]),
+  "DirL1+Prax2+NelM2 " => (pr, d) -> combine_nlopt_algs(pr, d; Algs = DPN, RelTimes = [1,2,2]),
+  "RandNLopt non-grad" => (pr, d) -> combine_nlopt_algs(pr, d),
+}
 
-r = combine_nlopt_algs(rosenbrock_of, 16)
-r = combine_nlopt_algs(rosenbrock_of, 32)
-r = combine_nlopt_algs(rosenbrock_of, 64)
-r = combine_nlopt_algs(rosenbrock_of, 512)
-r = combine_nlopt_algs(rastrigin_of, 512)
-r = combine_nlopt_algs(ackley_of, 512)
+function compare_and_save(opts, probs, dims, reps, mapper)
+  for dim in dims
+    res, filename = compare_optimizers(opts, probs, dim, reps; dataframe_mapper = mapper)
+    println("Wrote results to file: ", filename)
+  end
+end
+
+compare_and_save(Optimizers, Problems, [256, 1024], 1, dfmapper)
