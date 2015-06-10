198a62cfed75635910c43fa66ebb58fc3824e430
Alexey Stukalov <astukalov@gmail.com>
2015-06-10 15:33:36 +0200

    simplify and fix warnings in latin cube

--- src/utilities/latin_hypercube_sampling.jl
+++ src/utilities/latin_hypercube_sampling.jl
@@ -4,9 +4,9 @@ function latin_hypercube_sampling(mins, maxs, numSamples)
   result = zeros(numSamples, dims)
   for(i in 1:dims)
     interval_len = (maxs[i] - mins[i]) / numSamples
-    interval_starts = linspace(mins[i], maxs[i], numSamples+1)[1:numSamples]
-    samples_for_dim = interval_starts + interval_len * rand(numSamples, 1)
-    result[:,i] = samples_for_dim[shuffle([1:numSamples])]
+    samples_for_dim = linspace(mins[i], maxs[i] - interval_len, numSamples) +
+                      interval_len*rand(numSamples)
+    result[:,i] = samples_for_dim[shuffle(collect(1:numSamples))]
   end
   result
 end
