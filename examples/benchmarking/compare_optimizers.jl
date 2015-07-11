b592036996c53b35ae330d947711fcea2777d506
Robert Feldt <robert.feldt@gmail.com>
2015-07-11 22:28:38 +0200

    take git hash value from current dir

--- examples/benchmarking/compare_optimizers.jl
+++ examples/benchmarking/compare_optimizers.jl
@@ -200,7 +200,7 @@ function fitness_for_opt(family::FunctionBasedProblemFamily, numDimensions, popu
 end
 
 function latest_git_id()
-  strip(readall(`git -C "$(Pkg.dir("BlackBoxOptim"))" log --format="%H" -n 1`))
+  strip(readall(`git -C "../.." log --format="%H" -n 1`))
 end
 
 # Test an optimizer on multiple problems and dimensions, return results in dict.
