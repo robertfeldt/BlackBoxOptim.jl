79198cbf4da3b1ff81a03256d4d6d4cbc3226e13
Alexey Stukalov <astukalov@gmail.com>
2015-08-21 01:25:19 +0200

    improve NES
    - parameterize by fitness type and
      use vector of Candidate{F}s instead of matrix to eliminate
      some unnecessary parameters copying
    - parameterize by EmbeddingOperator and implement projecting of
      candidates onto the search space (closes #19)
    - optimize linear algebra to save some unnecessary memory/stack
      allocation and copying

--- src/natural_evolution_strategies.jl
+++ src/natural_evolution_strategies.jl
@@ -1,10 +1,7 @@
-using BlackBoxOptim
-using Distributions
-
 abstract NaturalEvolutionStrategyOpt <: PopulationOptimizer
 
-type SeparableNESOpt <: NaturalEvolutionStrategyOpt
-  d::Int                          # Number of dimensions
+type SeparableNESOpt{F,E<:EmbeddingOperator} <: NaturalEvolutionStrategyOpt
+  embed::E                        # operator embedding into the search space
   lambda::Int                     # Number of samples to take per iteration
   mu::Vector{Float64}             # Average position for sampling each position
   sigma::Vector{Float64}          # Average std deviation for sampling in position
@@ -12,40 +9,41 @@ type SeparableNESOpt <: NaturalEvolutionStrategyOpt
   mu_learnrate::Float64
   sigma_learnrate::Float64
   last_s::Array{Float64,2}        # The s values sampled in the last call to ask
-  population::PopulationMatrix    # The last sampled values, now being evaluated
+  candidates::Vector{Candidate{F}}# The last sampled values, now being evaluated
   utilities::Vector{Float64}      # The fitness shaping utility vector
 
-  function SeparableNESOpt(searchSpace; lambda::Int = 0, mu_learnrate::Float64 = 1.0,
-    sigma_learnrate::Float64 = 0.0, ini_x = nothing)
-
-    numDimensions = numdims(searchSpace)
-
-    sigma = ones(numDimensions)
-    distr = Normal(0, 1)
+  function SeparableNESOpt(embed::E;
+    lambda::Int = 0,
+    mu_learnrate::Float64 = 1.0,
+    sigma_learnrate::Float64 = 0.0,
+    ini_x = nothing)
+    d = numdims(search_space(embed))
 
     if lambda == 0
-      lambda = 4 + ceil(Int, log(3*numDimensions)) # default lambda
+      lambda = 4 + ceil(Int, log(3*d)) # default lambda
     end
     if sigma_learnrate == 0.0
-      sigma_learnrate = calc_sigma_learnrate_for_snes(numDimensions) # default sigma learn rate
+      sigma_learnrate = calc_sigma_learnrate_for_snes(d) # default sigma learn rate
     end
     if ini_x === nothing
-      ini_x = rand(numDimensions)
+      ini_x = rand_individual(search_space(embed))
     else
       ini_x = copy(ini_x::Individual)
     end
 
-    new(numDimensions, lambda, ini_x, sigma, distr,
+    new(embed, lambda,
+      ini_x, ones(d),
+      Normal(0, 1),
       mu_learnrate, sigma_learnrate,
-      zeros(numDimensions, lambda), zeros(numDimensions, lambda),
+      zeros(d, lambda),
+      Candidate{F}[Candidate{F}(Array(Float64, d), i) for i in 1:lambda],
       # Most modern NES papers use log rather than linear fitness shaping.
       fitness_shaping_utilities_log(lambda))
   end
 end
 
-
 # We use a different ordering of the dimensions than other optimizers, so transpose.
-population(o::NaturalEvolutionStrategyOpt) = o.population
+population(o::NaturalEvolutionStrategyOpt) = o.candidates
 
 const NES_DefaultOptions = @compat Dict{Symbol,Any}(
   :lambda => 0,              # If 0 it will be set based on the number of dimensions
@@ -56,7 +54,8 @@ const NES_DefaultOptions = @compat Dict{Symbol,Any}(
 
 function separable_nes(problem::OptimizationProblem, parameters)
   params = chain(NES_DefaultOptions, parameters)
-  SeparableNESOpt(search_space(problem),
+  embed = RandomBound(search_space(problem))
+  SeparableNESOpt{fitness_type(problem), typeof(embed)}(embed,
     lambda = params[:lambda],
     mu_learnrate = params[:mu_learnrate],
     sigma_learnrate = params[:sigma_learnrate],
@@ -71,23 +70,20 @@ function ask(snes::SeparableNESOpt)
   # Sample from N(0, 1)
   randn!(snes.last_s)
 
-  # Add in the mu and sigma's...
-  #sampled_solutions = repmat(snes.mu, 1, l) + repmat(snes.sigma, 1, l) .* s
-  # Quicker version as dimensions increase:
-  sampled_solutions = broadcast!(+, snes.population, snes.mu, broadcast(*, snes.sigma, snes.last_s))
-
-  mix_with_indices(sampled_solutions, 1:snes.lambda)
-end
-
-function mix_with_indices(individuals::Matrix{Float64}, indices::Range)
-  if popsize(individuals) != length(indices)
-    throw(DimensionMismatch("The number of candidates does not match the number of indices"))
+  for i in eachindex(snes.candidates)
+    candi = snes.candidates[i]
+    @inbounds for j in 1:length(candi.params)
+      candi.params[j] = snes.mu[j] + snes.sigma[j] * snes.last_s[j, i]
+    end
+    apply!(snes.embed, candi.params, snes.mu)
+    candi.fitness = NaN # FIXME: use nafitness()
   end
-  Candidate{Float64}[Candidate{Float64}(individuals[:,i], i) for i in indices]
+
+  return snes.candidates
 end
 
 # Tell the sNES the ranking of a set of candidates.
-function tell!{F}(snes::SeparableNESOpt, rankedCandidates::Vector{Candidate{F}})
+function tell!{F}(snes::SeparableNESOpt{F}, rankedCandidates::Vector{Candidate{F}})
   u = assign_weights(rankedCandidates, snes.utilities)
 
   # Calc gradient
@@ -96,79 +92,95 @@ function tell!{F}(snes::SeparableNESOpt, rankedCandidates::Vector{Candidate{F}})
   gradient_sigma = sq_s_minus1 * u
 
   # Update the mean and sigma vectors based on the gradient
-  snes.mu = snes.mu + snes.mu_learnrate * (snes.sigma .* gradient_mu)
-  snes.sigma = snes.sigma .* exp(snes.sigma_learnrate / 2 * gradient_sigma)
+  old_mu = copy(snes.mu)
+  snes.mu += snes.mu_learnrate * (snes.sigma .* gradient_mu)
+  snes.sigma .*= exp(snes.sigma_learnrate / 2 * gradient_sigma)
+  apply!(snes.embed, snes.mu, old_mu)
 
   # There is no notion of how many was better in NES so return 0
-  0
+  return 0
 end
 
-function assign_weights{F}(candidates::Vector{Candidate{F}}, u::Vector{Float64})
-  n = length(candidates)
-  # We must reorder the samples according to the order of the fitness in
-  # candidates!!! Or we must reorder the utilities accordingly. The latter
-  # is the preferred method and we can use the indices in candidates to
-  # accomplish it.
+# We must reorder the samples according to the order of the fitness in
+# candidates!!! Or we must reorder the utilities accordingly. The latter
+# is the preferred method and we can use the indices in candidates to
+# accomplish it.
+assign_weights{F}(candidates::Vector{Candidate{F}}, u::Vector{Float64}) =
   u[sortperm(candidates, by = fitness)]
-end
 
 # xNES is nice but scales badly with increasing dimension.
-type XNESOpt <: NaturalEvolutionStrategyOpt
-  d::Int                          # Number of dimensions
+type XNESOpt{F,E<:EmbeddingOperator} <: NaturalEvolutionStrategyOpt
+  embed::E                        # operator embedding into the search space
   lambda::Int                     # Number of samples to take per iteration
   utilities::Vector{Float64}      # Fitness utility to give to each rank
   x_learnrate::Float64
   a_learnrate::Float64
   A::Array{Float64,2}
   expA::Array{Float64,2}
-  population::PopulationMatrix    # The last sampled values, now being evaluated
+  candidates::Vector{Candidate{F}}# The last sampled values, now being evaluated
   x::Individual                   # The current incumbent (aka most likely value, mu etc)
   Z::Array{Float64,2}
 
-  XNESOpt(searchSpace; lambda::Int = 0, ini_x = nothing) = begin
-    d = numdims(searchSpace)
+  function XNESOpt(embed::E; lambda::Int = 0, ini_x = nothing)
+    d = numdims(search_space(embed))
     if lambda == 0
       lambda = 4 + 3*floor(Int, log(d))
     end
     if ini_x === nothing
-      ini_x = rand_individual(searchSpace)
+      ini_x = rand_individual(search_space(embed))
     else
       ini_x = copy(ini_x::Individual)
     end
-    x_learnrate = 1
-    a_learnrate = 0.5 * minimum([1.0 / d, 0.25])
 
-    new(d, lambda, fitness_shaping_utilities_log(lambda),
-      x_learnrate, a_learnrate,
-      zeros(d, d), zeros(d, d), zeros(d, lambda), ini_x, zeros(d, lambda))
+    new(embed, lambda, fitness_shaping_utilities_log(lambda),
+      1.0, 0.5 * min(1.0 / d, 0.25),
+      zeros(d, d), zeros(d, d),
+      Candidate{F}[Candidate{F}(Array(Float64, d), i) for i in 1:lambda],
+      ini_x, zeros(d, lambda))
   end
 end
 
 function xnes(problem::OptimizationProblem, parameters)
   params = chain(NES_DefaultOptions, parameters)
-  XNESOpt(search_space(problem); lambda = params[:lambda], ini_x = params[:ini_x])
+  embed = RandomBound(search_space(problem))
+  XNESOpt{fitness_type(problem), typeof(embed)}(embed; lambda = params[:lambda], ini_x = params[:ini_x])
 end
 
 function ask(xnes::XNESOpt)
-  xnes.expA = expm(xnes.A) # FIXME matrix allocation is expensive, use expm!() (when available)
+  copy!(xnes.expA, xnes.A)
+  Base.LinAlg.expm!(xnes.expA)
   randn!(xnes.Z)
-  broadcast!(+, xnes.population, xnes.x, (xnes.expA * xnes.Z))
+  expAZ = xnes.expA * xnes.Z
 
-  mix_with_indices(xnes.population, 1:xnes.lambda)
+  for i in eachindex(xnes.candidates)
+    candi = xnes.candidates[i]
+    @inbounds for j in 1:length(candi.params)
+      candi.params[j] = xnes.x[j] + expAZ[j, i]
+    end
+    apply!(xnes.embed, candi.params, xnes.x)
+    candi.fitness = NaN # FIXME: use nafitness()
+  end
+
+  return xnes.candidates
 end
 
-function tell!{F}(xnes::XNESOpt, rankedCandidates::Vector{Candidate{F}})
+function tell!{F}(xnes::XNESOpt{F}, rankedCandidates::Vector{Candidate{F}})
   u = assign_weights(rankedCandidates, xnes.utilities)
 
   # fixme improve memory footprint by using A_mul_B!() etc
-  G = broadcast(*, u', xnes.Z) * xnes.Z' - eye(xnes.d)
-  dx = xnes.x_learnrate * xnes.expA * (xnes.Z*u)
-  dA = xnes.a_learnrate * G
-
-  xnes.x += dx
+  dA = A_mul_Bt(broadcast(*, u', xnes.Z), xnes.Z)
+  for i in 1:numdims(population(xnes))
+    dA[i,i] -= 1.0
+  end
+  dA *= xnes.a_learnrate
   xnes.A += dA
 
-  0
+  old_x = copy(xnes.x)
+  dx = xnes.x_learnrate * xnes.expA * (xnes.Z*u)
+  xnes.x += dx
+  apply!(xnes.embed, xnes.x, old_x)
+
+  return 0
 end
 
 # Calculate the fitness shaping utilities vector using the log method.
--- test/test_natural_evolution_strategies.jl
+++ test/test_natural_evolution_strategies.jl
@@ -2,20 +2,7 @@ using FactCheck
 
 facts("sNES") do
 
-context("mix_with_indices") do
-  cs = randn(2,3)
-  @fact_throws DimensionMismatch BlackBoxOptim.mix_with_indices(cs, 1:2)
-  r = BlackBoxOptim.mix_with_indices(cs, 1:3)
-  @fact length(r) => 3
-  @fact r[1].index => 1
-  @fact r[2].index => 2
-  @fact r[3].index => 3
-  @fact r[1].params => cs[:,1]
-  @fact r[2].params => cs[:,2]
-  @fact r[3].params => cs[:,3]
-end
-
-function assign_weights{F}(fitnesses::Vector{F})
+function assign_weights_wrapper{F}(fitnesses::Vector{F})
   candidates = BlackBoxOptim.Candidate{F}[BlackBoxOptim.Candidate{F}([0.0], -1, f) for f in fitnesses]
   u = BlackBoxOptim.fitness_shaping_utilities_linear(length(candidates))
   BlackBoxOptim.assign_weights(candidates, u)
@@ -23,25 +10,25 @@ end
 
 context("assign_weights") do
   context("when indices are already ordered") do
-    u = assign_weights([1.0, 2.0])
+    u = assign_weights_wrapper([1.0, 2.0])
     @fact length(u) => 2
     @fact u[1] => 1.0
     @fact u[2] => 0.0
 
-    u = assign_weights([1.0, 2.0, 3.0])
+    u = assign_weights_wrapper([1.0, 2.0, 3.0])
     @fact length(u) => 3
     @fact u[1] => 1.0
     @fact u[2] => 0.0
     @fact u[3] => 0.0
 
-    u = assign_weights([1, 2, 3, 4])
+    u = assign_weights_wrapper([1, 2, 3, 4])
     @fact length(u) => 4
     @fact u[1] => roughly(2/3)
     @fact u[2] => roughly(1/3)
     @fact u[3] => 0.0
     @fact u[4] => 0.0
 
-    u = assign_weights([1, 2, 3, 4, 5])
+    u = assign_weights_wrapper([1, 2, 3, 4, 5])
     @fact length(u) => 5
     @fact u[1] => roughly(2/3)
     @fact u[2] => roughly(1/3)
@@ -49,7 +36,7 @@ context("assign_weights") do
     @fact u[4] => 0.0
     @fact u[5] => 0.0
 
-    u = assign_weights([1, 2, 3, 4, 5, 6])
+    u = assign_weights_wrapper([1, 2, 3, 4, 5, 6])
     @fact length(u) => 6
     @fact u[1] => roughly((3/3)/(3/3+2/3+1/3))
     @fact u[2] => roughly((2/3)/(3/3+2/3+1/3))
@@ -60,7 +47,7 @@ context("assign_weights") do
   end
 
   context("when indices are not ordered") do
-    u = assign_weights([4, 1, 2, 3])
+    u = assign_weights_wrapper([4, 1, 2, 3])
     @fact u[4] => roughly(2/3)
     @fact u[1] => roughly(1/3)
     @fact u[2] => 0.0
