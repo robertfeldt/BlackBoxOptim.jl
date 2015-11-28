ab005d9934b4150a007a081bfa5187d993d814cc
Robert Feldt <robert.feldt@gmail.com>
2015-11-28 13:19:49 +0100

    example now shows benefit of parallel eval by slowing down func to be optimized

--- examples/rosenbrock_parallel.jl
+++ examples/rosenbrock_parallel.jl
@@ -5,18 +5,30 @@ addprocs(2)
 # note that BlackBoxOptim gets automatically used on all workers
 using BlackBoxOptim
 
-# define the function to optimize on all workers
+# define the function to optimize on all workers. Parallel eval only gives a gain
+# if function to optimize is slow. For this example we introduce a fake sleep
+# to make it slow since the function is actually very quick to eval...
 @everywhere function rosenbrock(x)
+  sleep(0.0005) # Fake a slower func to be optimized...
   return( sum( 100*( x[2:end] - x[1:end-1].^2 ).^2 + ( x[1:end-1] - 1 ).^2 ) )
 end
 
 # First run without any parallel procs used in eval
 opt1 = bbsetup(rosenbrock; Method=:xnes, SearchRange = (-5.0, 5.0),
-               NumDimensions = 100, MaxSteps = 5000)
+               NumDimensions = 50, MaxFuncEvals = 5000)
+tic()
 res1 = bboptimize(opt1)
+t1 = toq()
 
 # When Workers= option is given, BlackBoxOptim enables parallel
 # evaluation of fitness using the specified worker processes
 opt2 = bbsetup(rosenbrock; Method=:xnes, SearchRange = (-5.0, 5.0),
-               NumDimensions = 100, MaxSteps = 5000, Workers = workers())
+               NumDimensions = 50, MaxFuncEvals = 5000, Workers = workers())
+tic()
 res2 = bboptimize(opt2)
+t2 = toq()
+
+println("Time: serial = $(t1), parallel = $(t2) (difference = $(t2-t1))")
+if t2 < t1
+  println("Speedup is $(round(t1/t2, 1))x")
+end
