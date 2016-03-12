d279a088783ad6704f8e703411c3051a1c7e42da
Robert Feldt <robert.feldt@gmail.com>
2016-03-12 10:57:00 +0100

    adding script to update with new set of benchmarks

---
+++ examples/benchmarking/runupdate.jl
@@ -0,0 +1,12 @@
+nbfile = Libc.strftime("benchmark_runs_%Y%m%d.csv", time())
+
+if isfile(nbfile)
+  println("Deleting existing file: ", nbfile)
+  rm(nbfile)
+end
+
+nreps = (length(ARGS) > 0 ? parse(Int, ARGS[1]) : 10)
+
+cmd = `julia --color=yes -L ../../src/BlackBoxOptim.jl compare_optimizers.jl update --benchmarkfile $nbfile --numreps $nreps`
+
+run(cmd)
