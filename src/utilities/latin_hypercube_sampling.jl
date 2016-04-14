cbde7e65cfb65bba043986e8b2da6ab15a66b2d8
Alexey Stukalov <astukalov@gmail.com>
2016-04-15 01:25:28 +0200

    latin_hypercube_sampling(): refactor
    also fix 0.5 for syntax

--- src/utilities/latin_hypercube_sampling.jl
+++ src/utilities/latin_hypercube_sampling.jl
@@ -1,15 +1,16 @@
 """
-  Randomly sample `numSamples` values from the parallelogram defined
-  by `mins` and `maxs` using the Latin hypercube algorithm.
+    latin_hypercube_sampling(mins, maxs, numSamples)
+
+    Randomly sample `numSamples` values from the parallelogram defined
+    by `mins` and `maxs` using the Latin hypercube algorithm.
 """
-function latin_hypercube_sampling(mins, maxs, numSamples)
-  dims = length(mins)
-  result = zeros(numSamples, dims)
-  for(i in 1:dims)
-    interval_len = (maxs[i] - mins[i]) / numSamples
-    samples_for_dim = linspace(mins[i], maxs[i] - interval_len, numSamples) +
-                      interval_len*rand(numSamples)
-    result[:,i] = samples_for_dim[shuffle(collect(1:numSamples))]
-  end
-  return result'
+function latin_hypercube_sampling{T}(mins::AbstractVector{T}, maxs::AbstractVector{T}, numSamples::Integer)
+    dims = length(mins)
+    result = zeros(T, numSamples, dims)
+    @inbounds for i in 1:dims
+        interval_len = (maxs[i] - mins[i]) / numSamples
+        result[:,i] = shuffle!(linspace(mins[i], maxs[i] - interval_len, numSamples) +
+                               interval_len*rand(numSamples))
+    end
+    return result'
 end
