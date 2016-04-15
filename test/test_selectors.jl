152667438575a4a98f77d6e40cd15b46c28c9103
Alexey Stukalov <astukalov@gmail.com>
2016-04-15 14:36:04 +0200

    TournamentSelector: fix/optimize and add tests
    - use the proper comparison (the winning result is -1)
    - don't do all possible pairs -- wastes too much resources and
      not necessary for random candidates selection
    - add tests

--- test/test_selectors.jl
+++ test/test_selectors.jl
@@ -37,4 +37,37 @@ context("RadiusLimitedSelector") do
   end
 end
 
+context("Tournament") do
+    sel = BlackBoxOptim.TournamentSelector(MinimizingFitnessScheme, 3)
+    fake_pop = FitPopulation(collect(1.0:10.0)', NaN, collect(1.0:10.0))
+
+    context("tournament()") do
+        for i in 1:NumTestRepetitions
+            tourSize = rand(0:popsize(fake_pop))
+            cand_ixs = find(bitrand(popsize(fake_pop)))
+
+            winner_ix = BlackBoxOptim.tournament(sel, fake_pop, cand_ixs)
+
+            if isempty(cand_ixs)
+                @fact winner_ix --> 0
+            else
+                @fact in(winner_ix, 1:popsize(fake_pop)) --> true
+                @fact fitness(fake_pop, winner_ix) --> minimum(fake_pop.fitness[cand_ixs])
+            end
+        end
+    end
+
+    context("select()") do
+        for i in 1:NumTestRepetitions
+            numSamples = rand(1:3)
+            sampled = BlackBoxOptim.select(sel, fake_pop, numSamples)
+
+            @fact length(sampled) --> numSamples
+
+            # All sampled indices are indices into the population
+            @fact all(index -> in(index, 1:popsize(fake_pop)), sampled) --> true
+        end
+    end
+end
+
 end
