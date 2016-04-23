8978039de45f295bb9149fdf8a56086041a7f336
Alexey Stukalov <astukalov@gmail.com>
2016-04-24 00:06:33 +0200

    EpsBoxArchive: add "simulate optimization" test

--- test/test_epsbox_archive.jl
+++ test/test_epsbox_archive.jl
@@ -1,4 +1,20 @@
 facts("EpsBoxArchive") do
+    # check that frontier elements are mutually nondominated and
+    # ther are no epsilon-index duplicates
+    function check_frontier(a::EpsBoxArchive)
+        for (i, el_i) in enumerate(pareto_frontier(a))
+            for (j, el_j) in enumerate(pareto_frontier(a))
+                if j > i
+                    comp, index_match = hat_compare(fitness(el_i), fitness(el_j), fitness_scheme(a))
+                    if comp != 0 || index_match
+                        return comp, index_match # return if violating pair is found
+                    end
+                end
+            end
+        end
+        return (0, false) # all non-dominated, no index matches
+    end
+
   scheme = EpsBoxDominanceFitnessScheme{2}(0.1, is_minimizing=true)
 
   context("EpsBoxFrontierIndividual") do
@@ -140,4 +156,31 @@ facts("EpsBoxArchive") do
     @fact a.frontier[1].params --> [1.5, 2.0]
   end
 
+  context("simulate optimization") do
+    # generate random fitness from (0..2, 0..2, 0..2)
+    # and lying inside the (1.0-r1)^1.5 + (1.0-r2)^1.5 + (1.0-r3)^1.5 <= 8.0
+    function rfitness()
+        while true
+            r1 = rand()
+            r2 = rand()
+            r3 = rand()
+            if (1.0-r1)^1.5 + (1.0-r2)^1.5 + (1.0-r3)^1.5 <= 1.0
+                return (2.0*r1, 2.0*r2, 2.0*r3)
+            end
+        end
+    end
+
+    scheme3 = EpsBoxDominanceFitnessScheme{3}(0.1, is_minimizing=true)
+    a = EpsBoxArchive(scheme3, max_size=1000)
+    indiv = [0.0, 0.0]
+    for i in 1:10000
+        BlackBoxOptim.add_candidate!(a, rfitness(), indiv)
+        # periodically check that pareto_frontier(a) is indeed
+        # epsilon-index frontier
+        if mod(i, 100) == 0
+            @fact length(a) --> isposdef
+            @fact check_frontier(a) --> (0, false)
+        end
+    end
+  end
 end
