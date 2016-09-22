f5ca94f234293bb01a3c5a20063094eea528d110
Alexey Stukalov <astukalov@gmail.com>
2016-09-22 11:14:08 +0200

    refactor bimodal Cauchy distribution
    - avoid recursion when sampling
    - avoid keyword arguments when sampling

--- test/test_bimodal_cauchy_distribution.jl
+++ test/test_bimodal_cauchy_distribution.jl
@@ -1,25 +1,31 @@
 @testset "Bimodal Cauchy Distributions" begin
 
-    @testset "sample bimodal cauchy with truncation on one side" begin
-        bc = BlackBoxOptim.bimodal_cauchy(0.65, 0.1, 1.0, 0.1)
+    @testset "sample bimodal cauchy with truncation above 1" begin
+        bc = BlackBoxOptim.BimodalCauchy(0.65, 0.1, 1.0, 0.1, clampBelow0=false)
 
-        for i in 1:100
-            v = BlackBoxOptim.sample_bimodal_cauchy(bc; truncateAbove1 = true, truncateBelow0 = false)
-
-            @test v <= 1.0
-            @test v > 0.0 # Very unlikely to be 0.0 so should be ok...
+        n_gt0 = 0
+        n_le1 = 0
+        for _ in 1:10000
+            v = rand(bc)
+            (v > 0.0) && (n_gt0 += 1) # Very unlikely to be 0.0 so should be ok...
+            (v <= 1.0) && (n_le1 += 1)
         end
+        @test n_gt0 == 10000
+        @test n_le1 == 10000
     end
 
-    @testset "sample bimodal cauchy with truncation on both sides" begin
-        bc = BlackBoxOptim.bimodal_cauchy(0.1, 0.1, 0.95, 0.1)
+    @testset "sample bimodal cauchy with truncation below 0 and above 1" begin
+        bc = BlackBoxOptim.BimodalCauchy(0.1, 0.1, 0.95, 0.1)
 
-        for i in 1:100
-            v = BlackBoxOptim.sample_bimodal_cauchy(bc; truncateAbove1 = true, truncateBelow0 = true)
-
-            @test v <= 1.0
-            @test v >= 0.0
+        n_ge0 = 0
+        n_le1 = 0
+        for _ in 1:10000
+            v = rand(bc)
+            (v >= 0.0) && (n_ge0 += 1)
+            (v <= 1.0) && (n_le1 += 1)
         end
+        @test n_ge0 == 10000
+        @test n_le1 == 10000
     end
 
 end
