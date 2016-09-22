ea5774ff8d3e3462164e9a3177fc50314263f6c1
Alexey Stukalov <astukalov@gmail.com>
2016-09-22 11:13:51 +0200

    add tests for accessing population individuals

--- test/test_population.jl
+++ test/test_population.jl
@@ -11,6 +11,30 @@
         @test isnafitness(fitness(p1, 1), fs)
         @test isnafitness(fitness(p1, 4), fs)
 
+        @testset "accessing individuals" begin
+    		@test isa(p1[1], Vector{Float64})
+            @test length(p1[1]) == numdims(p1)
+            @test isa(BlackBoxOptim.viewer(p1, 1), AbstractVector{Float64})
+            @test length(BlackBoxOptim.viewer(p1, 1)) == numdims(p1)
+
+            @test isa(p1[popsize(p1)], Vector{Float64}) # last solution vector
+            @test_throws BoundsError p1[0]
+            @test_throws BoundsError p1[popsize(p1)+1]
+            @test_throws BoundsError BlackBoxOptim.viewer(p1, 0)
+            @test_throws BoundsError BlackBoxOptim.viewer(p1, popsize(p1)+1)
+            rand_solution_idx = rand(2:(popsize(p1)-1))
+            @test isa(p1[rand_solution_idx], Array{Float64, 1}) # random solution vector
+        end
+
+        @testset "accessing individuals fitness" begin
+            # and to access their fitness values:
+            @test isa(fitness(p1, 1), Float64)
+            @test isa(fitness(p1, popsize(p1)), Float64)
+            rand_solution_idx = rand(2:(popsize(p1)-1))
+            @test_throws BoundsError fitness(p1, 0)
+            @test_throws BoundsError fitness(p1, popsize(p1)+1)
+        end
+
         @testset "candidates pool" begin
             @test BlackBoxOptim.candi_pool_size(p1) == 0
             candi1 = BlackBoxOptim.acquire_candi(p1, 1)
