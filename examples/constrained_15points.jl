0973b35d23c04a6650cee6c3028ac7c00b4adf84
Robert Feldt <robert.feldt@gmail.com>
2017-07-20 07:45:38 +0200

    fixed also constrained_15points example to use latest bboptimize interface

--- examples/constrained_15points.jl
+++ examples/constrained_15points.jl
@@ -23,16 +23,7 @@ prob = BlackBoxOptim.minimization_problem(points15, "Constrained points 15",
 # This is a hard problem so run for some time...
 MaxMinutes = 0.50
 
-# Run several methods several times:
-#BlackBoxOptim.repeated_bboptimize(2, prob, Dims, [
-#  :generating_set_search,
-#  :adaptive_de_rand_1_bin_radiuslimited,
-#  #:random_search,
-#  #:separable_nes # Can't apply separable NES since it does not yet respect box constraints...
-#  ],
-#  MaxMinutes * 60)
-
-# To run XNES on it we need to add a penalty for going outside the (0,1) box.
+# To run XNES et al on it we need to add a penalty for going outside the (0,1) box.
 function penalty(x, range)
   lower = x .< range[1]
   higher = x .> range[2]
@@ -42,6 +33,22 @@ end
 penalized_points15(x) = points15(x) + penalty(x, (0.0, 1.0))
 
 # Run XNES
-best, fitness = bboptimize(penalized_points15; SearchRange = (0.0, 1.0), NumDimensions = Dims,
+result = bboptimize(penalized_points15; SearchRange = (0.0, 1.0), NumDimensions = Dims,
   MaxTime = MaxMinutes * 60, Method = :xnes)
-println("xNES points15 fitness = ", points15(best))
+best_xnes = best_candidate(result)
+
+# Run DXNES
+result = bboptimize(penalized_points15; SearchRange = (0.0, 1.0), NumDimensions = Dims,
+  MaxTime = MaxMinutes * 60, Method = :dxnes)
+best_dxnes = best_candidate(result)
+
+# Run several methods several times:
+BlackBoxOptim.repeated_bboptimize(2, prob, Dims, [
+  :generating_set_search,
+  :adaptive_de_rand_1_bin_radiuslimited,
+  :random_search,
+  ],
+  MaxMinutes * 60)
+
+println("xNES points15 fitness = ", points15(best_xnes))
+println("dxNES points15 fitness = ", points15(best_dxnes))
