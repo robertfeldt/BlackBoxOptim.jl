450949ab7f67f39606a27e3a4f209d2e792aa3eb
Robert Feldt <robert.feldt@gmail.com>
2017-07-22 15:25:09 +0200

    example of how to optimize a function returning integers

---
+++ examples/cannot_opt_int_functions.jl
@@ -0,0 +1,22 @@
+using BlackBoxOptim
+
+# The functions being optimized need to return a float so trying
+# to optimize this function will throw an error:
+function int_returning_opt_func(x::Vector{Float64})
+    round(Int, sumabs(x))
+end
+
+# Throws an error if uncommented:
+try
+    bboptimize(int_returning_opt_func;
+        SearchRange = (-5.0, 5.0), NumDimensions = 3, TraceMode = :silent)
+catch err
+    println("Error raised: ", err)
+end
+
+# Solution is to just return a Float64 instead:
+fixed_func(x::Vector{Float64}) = float(int_returning_opt_func(x))
+
+res = bboptimize(fixed_func;
+        SearchRange = (-5.0, 5.0), NumDimensions = 3)
+println("Best fitness: ", best_fitness(res))
