be119a5630764f844bf2513ddb2f4c5c3a1651b7
Alexey Stukalov <astukalov@gmail.com>
2018-04-15 15:20:54 +0200

    pareto_dominates(): better var names

--- src/fitness/pareto_dominance.jl
+++ src/fitness/pareto_dominance.jl
@@ -2,33 +2,33 @@ pareto_dominates_clear(u::Vector{T}, v::Vector{T}) where {T <: Real} =
     all(u .<= v) && any(u .< v)
 
 function pareto_dominates_fast(u::Vector{T}, v::Vector{T}) where {T <: Real}
-    one_smaller = false
+    anylt = false
     @inbounds for i in eachindex(u)
         if u[i] > v[i]
             return false
         elseif u[i] < v[i]
-            one_smaller = true
+            anylt = true
         end
     end
-    return one_smaller
+    return anylt
 end
 
 pareto_dominates(v1::Vector{T}, v2::Vector{T}) where {T <: Real} =
     pareto_dominates_fast(v1, v2)
 
 function pareto_dominates_hat(u::Vector{T}, v::Vector{T}) where {T <: Real}
-    one_larger = one_smaller = false
+    anygt = anylt = false
     @inbounds for i in eachindex(u)
         if u[i] > v[i]
-            one_larger = true
+            anygt = true
         elseif u[i] < v[i]
-            one_smaller = true
+            anylt = true
         end
     end
-    if one_larger
-        return (one_smaller ? 0 : 1)
+    if anygt
+        return anylt ? 0 : 1
     else
-        return (one_smaller ? -1 : 0)
+        return anylt ? -1 : 0
     end
 end
 
