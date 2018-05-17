688f193dde8e45c09a6c2db8be7dc588105e4d20
Robert Feldt <robert.feldt@gmail.com>
2018-05-17 09:10:46 +0200

    fixed deprecation warnings, colour output for percent differences

--- examples/benchmarking/compare_testtimings.jl
+++ examples/benchmarking/compare_testtimings.jl
@@ -1,8 +1,13 @@
-using DataFrames
+using DataFrames, CSV
 
-Timings = readtable("test/timing_testing.csv")
+Timings = CSV.read("test/timing_testing.csv")
 
-res = by(Timings, :Julia, d1 -> by(d1, :Git, d2 -> by(d2, :TestFile, d3 -> mean(d3[:Elapsed]))))
+LatestJulia = Timings[end, :Julia]
+
+res = by(Timings, :Julia, 
+    d1 -> by(d1, :Git, 
+        d2 -> by(d2, :TestFile, 
+            d3 -> mean(d3[:Elapsed]))))
 
 # Slice based on testfiles and save with their largest time difference factor:
 res2 = map(unique(res[:, :TestFile])) do testfile
@@ -14,23 +19,33 @@ res2 = map(unique(res[:, :TestFile])) do testfile
     (testfile, slicedf, sorton)
 end
 
-function percent_diff(origvalue, currvalue)
+function print_percent_diff(origvalue, currvalue; rev=false)
     if currvalue > origvalue
-        "+" * string(round(100.0 * (currvalue/origvalue - 1.0), 1)) * "%"
+        s = "+" * string(round(100.0 * (currvalue/origvalue - 1.0), 1)) * "%"
+        color = rev ? :red : :gren
     else
-        "-" * string(round(100.0 * (1.0 - currvalue/origvalue), 1)) * "%"
+        s = "-" * string(round(100.0 * (1.0 - currvalue/origvalue), 1)) * "%"
+        color = rev ? :green : :red
     end
+    print_with_color(color, s)
 end
 
 # Sort by largest time diff:
-for (testfile, slicedf, diff) in sort(res2, by = t -> t[3], rev=true)
-    sorteddf = sort(slicedf, cols=[:x1])
+for (testfile, slicedf, diff) in sort(res2, by = t -> t[3], rev=false)
+    sorteddf = sort(slicedf, [:x1])
     mintime = sorteddf[1, :x1]
     mindesc = join(convert(Array, sorteddf[1, 1:2]), ", ")
     println(testfile, " (fastest = ", round(mintime, 2), " secs for ", mindesc, "):")
     for i in 2:nrow(sorteddf)
+        if sorteddf[i, :Julia] == LatestJulia
+            print("  * ")
+        else
+            print("    ")
+        end
         desc = join(convert(Array, sorteddf[i, 1:(ncol(sorteddf)-1)]), ", ")
-        println("  ", desc, ": ", round(sorteddf[i, :x1], 2), " (", percent_diff(mintime, sorteddf[i, :x1]), ")")
+        print(desc, ": ", round(sorteddf[i, :x1], 2), " (")
+        print_percent_diff(mintime, sorteddf[i, :x1]; rev=true)
+        println(")")
     end
     println("")
 end
