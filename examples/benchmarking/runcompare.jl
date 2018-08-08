ae59d0371a3b9bdf344b2dfa2974ba17c9cde325
Alexey Stukalov <astukalov@gmail.com>
2018-08-08 22:57:54 +0200

    Rakefile: fix benchmarking
    - add bench07 -- benchmarking on julia 0.7
    - add benchnew07 -- creating new benchmarks on julia 0.7 without comparison

--- examples/benchmarking/runcompare.jl
+++ examples/benchmarking/runcompare.jl
@@ -1,11 +1,20 @@
-bfiles = filter(f->typeof(match(r"^benchmark_runs_\d.+\.csv$", f))!=Nothing, readdir("."))
+scripts_dir = @__DIR__
+reports_dir = (length(ARGS) > 0 ? ARGS[1] : "examples/benchmarking")
 
-bf = bfiles[end]
+isdir(reports_dir) || throw(error("Benchmark results folder $reports_dir not found"))
+
+bfiles = filter!(f->ismatch(r"^benchmark_runs_\d.+\.csv$", f), readdir(reports_dir))
+
+isempty(bfiles) && throw(error("No benchmark runs found"))
+bf = joinpath(reports_dir, bfiles[end])
 
 println("Comparing to benchmark: ", bf)
 
-nreps = (length(ARGS) > 0 ? parse(Int, ARGS[1]) : 10)
+nreps = (length(ARGS) > 1 ? parse(Int, ARGS[2]) : 10)
 
-cmd = `julia --color=yes -L ../../src/BlackBoxOptim.jl compare_optimizers.jl compare --benchmarkfile $bf --numreps $nreps`
+
+@info @__DIR__
+
+cmd = `$(Sys.BINDIR)/julia --color=yes -L $scripts_dir/activate_pwd_env.jl -- $scripts_dir/compare_optimizers.jl compare --benchmarkfile $bf --numreps $nreps`
 
 run(cmd)
