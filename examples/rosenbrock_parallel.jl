c253bdda352ce7b564844663e87c940f0bdde840
Johan Blåbäck <johan_bluecreek@riseup.net>
2018-11-20 16:18:56 +0100

    Fix parallel example
    Removes the deprecated tic() and toc() in favour of @elapsed, distributes
    BlackBoxOptim to all workers, and includes the Distributed package for
    parallel evaluation.

--- examples/rosenbrock_parallel.jl
+++ examples/rosenbrock_parallel.jl
@@ -1,9 +1,11 @@
+using Distributed
+
 # Now add 2 procs that can exec in parallel (obviously it depends on your CPU
 # what you actually gain from this though)
 addprocs(2)
 
-# note that BlackBoxOptim gets automatically used on all workers
-using BlackBoxOptim
+# BlackBoxOptim does not get automatically used on all workers
+@everywhere using BlackBoxOptim
 
 # define the function to optimize on all workers. Parallel eval only gives a gain
 # if function to optimize is slow. For this example we introduce a fake sleep
@@ -16,17 +18,15 @@ end
 # First run without any parallel procs used in eval
 opt1 = bbsetup(slow_rosenbrock; Method=:xnes, SearchRange = (-5.0, 5.0),
                NumDimensions = 50, MaxFuncEvals = 5000)
-tic()
-res1 = bboptimize(opt1)
-t1 = round(toq(), digits=3)
+el1 = @elapsed res1 = bboptimize(opt1)
+t1 = round(el1, digits=3)
 
 # When Workers= option is given, BlackBoxOptim enables parallel
 # evaluation of fitness using the specified worker processes
 opt2 = bbsetup(slow_rosenbrock; Method=:xnes, SearchRange = (-5.0, 5.0),
                NumDimensions = 50, MaxFuncEvals = 5000, Workers = workers())
-tic()
-res2 = bboptimize(opt2)
-t2 = round(toq(), digits=3)
+el2 = @elapsed res2 = bboptimize(opt2)
+t2 = round(el2, digits=3)
 
 println("Time: serial = $(t1)s, parallel = $(t2)s")
 if t2 < t1
