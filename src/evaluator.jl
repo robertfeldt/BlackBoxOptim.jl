ec1832ddf0acfcfaaed0d670ab617517b45fc068
Alexey Stukalov <astukalov@gmail.com>
2019-12-29 01:36:00 +0100

    add AbstractAsyncEvaluator

--- src/evaluator.jl
+++ src/evaluator.jl
@@ -52,6 +52,67 @@ update_fitness!(e::Evaluator, candidates::Any; force::Bool=false) =
     update_fitness!(nothing, e, candidates, force=force)
 
 """
+The abstract base type for asynchronous evaluators.
+
+Asynchronous evaluator allows submitting fitness calculation requests via
+[`async_update_fitness`](@ref) method that returns *jobid* without waiting for
+the calculation completion. Job Ids could then be passed to [`sync_update_fitness!`](@ref)
+that waits until fitnesses for all specified jobs is calculated.
+"""
+abstract type AbstractAsyncEvaluator{P <: OptimizationProblem} <: Evaluator{P} end
+
+"""
+    async_update_fitness(eval::AbstractAsyncEvaluator, candi::Candidate;
+                         force::Bool=false, wait::Bool=false) -> Int
+
+Asynchronously calculate the fitness of a candidate.
+
+Returns
+  - the id of the fitness calculation job
+  - *0* if the candidate already has fitness
+  - *-1* if no fitness calculation was scheduled (`wait` unset, and all resources occupied)
+  - *-2* if evaluator does not accept jobs because it's shutting down
+
+# Arguments
+ - `force::Bool`: force fitness calculation even if the candidate already has
+   non-NA fitness
+ - `wait::Bool`: if all calculation resources are occupied, wait until the
+   calculation slot becomes available
+
+See also: [`sync_update_fitness!`](@ref), [`is_fitness_ready`](@ref)
+"""
+async_update_fitness
+
+"""
+    sync_fitness_update!([f], eval::AbstractAsyncEvaluator, jobids)
+
+Waits until all fitness `jobids` are calculated and optionally applies `f`
+function to each processed candidate.
+
+See also: [`async_update_fitness`](@ref), [`update_fitness!`](@ref).
+"""
+sync_update_fitness!
+
+"""
+    is_fitness_ready(eval::AbstractAsyncEvaluator, jobid::Integer) -> Bool
+
+Check if given asynchronous fitness job calculation is complete.
+
+See also [`async_update_fitness`](@ref), [`sync_update_fitness!`](@ref).
+"""
+is_fitness_ready
+
+"""
+    is_stopping(eval::AbstractAsyncEvaluator) -> Bool
+
+Check if the evaluator is in the shutdown sequence.
+
+When the evaluator is being shut down, it waits for the workers to finish
+their jobs and stop, no new jobs could be submitted.
+"""
+is_stopping
+
+"""
 Default implementation of the `Evaluator`.
 
 `FP` is the original problem's fitness type
