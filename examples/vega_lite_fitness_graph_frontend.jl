45a322236cb4749467f091471bc4ba380555a1d2
Robert Feldt <robert.feldt@gmail.com>
2020-03-19 12:51:08 +0100

    use rosenbrock for example since it gives smoother fitness progression so shows the interface in a better way

--- examples/vega_lite_fitness_graph_frontend.jl
+++ examples/vega_lite_fitness_graph_frontend.jl
@@ -12,15 +12,14 @@ function callback(oc)
 end
 
 # Func to optimize.
-function rastrigin(x)
-    D = length(x)
-    10 * D + sum(abs2, x) - 10 * sum(xx -> cos(2π * xx), x)
+function rosenbrock(x)
+    sum(i -> 100*abs2(x[i+1] - x[i]^2) + abs2(x[i] - 1), Base.OneTo(length(x)-1))
 end
 
 # Now optimize for 2 minutes. 
 # Go to http://127.0.0.1:8081 to view fitness progress!
-res = bboptimize(rastrigin;
-        SearchRange=(-5.12,5.12), NumDimensions = 50,
+res = bboptimize(rosenbrock;
+        SearchRange=(-10.0,10.0), NumDimensions = 500,
         PopulationSize=100, MaxTime=2*60.0,
         CallbackFunction = callback, CallbackInterval = 2.0);
 println("Best fitness = ", best_fitness(res))
