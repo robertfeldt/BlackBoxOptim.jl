a27497a32f81a4612958bd77a42e3f38900d5b02
Alexey Stukalov <astukalov@gmail.com>
2020-04-01 02:14:34 +0200

    fix whitespace

--- src/gui/vega_lite_fitness_graph.jl
+++ src/gui/vega_lite_fitness_graph.jl
@@ -9,17 +9,17 @@ const VegaLiteWebsocketFrontEndTemplate = """
 	<script src="https://cdn.jsdelivr.net/npm/vega-embed@3"></script>
 </head>
 
-<body> 
+<body>
   <div id="vis"></div>
-  
-  <script>    
+
+  <script>
     const vegalitespec = %%VEGALITESPEC%%;
     vegaEmbed('#vis', vegalitespec, {defaultStyle: true})
     .then(function(result) {
         const view = result.view;
         const port = %%SOCKETPORT%%;
         const conn = new WebSocket("ws://127.0.0.1:" + port);
-    
+
         conn.onopen = function(event) {
           // insert data as it arrives from the socket
           conn.onmessage = function(event) {
@@ -52,7 +52,7 @@ const VegaLiteFitnessOverTimeSpecTemplate = """
             "type": "quantitative"
         },
         "y": {
-            "field": "Fitness", 
+            "field": "Fitness",
             "type": "quantitative",
             "scale": {"type": "log"}
         }
@@ -62,7 +62,7 @@ const VegaLiteFitnessOverTimeSpecTemplate = """
 
 rand_websocket_port() = 9000 + rand(0:42)
 
-function vegalite_websocket_frontend(vegalitespec::String = VegaLiteFitnessOverTimeSpecTemplate; 
+function vegalite_websocket_frontend(vegalitespec::String = VegaLiteFitnessOverTimeSpecTemplate;
     width::Int = 800, height::Int = 600, port::Int = rand_websocket_port())
     res = replace(VegaLiteWebsocketFrontEndTemplate, "%%VEGALITESPEC%%" => vegalitespec)
     res = replace(res, "%%WIDTH%%" => string(width))
@@ -81,7 +81,7 @@ function serve_html_file(file::String)
 end
 
 # Serve a VegaLite front-end on html and then push updates to data
-# over a websocket so that the VegaLite graph is updated. 
+# over a websocket so that the VegaLite graph is updated.
 mutable struct VegaLiteGraphFitnessGraph
     verbose::Bool
     httpport::Int
@@ -90,7 +90,7 @@ mutable struct VegaLiteGraphFitnessGraph
     data::Vector{Any}
     last_sent_index::Int
     starttime::Float64
-    function VegaLiteGraphFitnessGraph(verbose::Bool = true, 
+    function VegaLiteGraphFitnessGraph(verbose::Bool = true,
         httpport::Int = 8081, websocketport::Int = 9000, mindelay::Float64 = 1.0)
         @assert mindelay > 0.0
         new(verbose, httpport, websocketport, mindelay, Any[], 0, 0.0)
