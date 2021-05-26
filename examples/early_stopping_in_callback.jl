6331ea5532cb5fd5c02bc7ad2eeb4d2da135e874
Robert Feldt <robert.feldt@gmail.com>
2021-05-26 07:07:14 +0200

    example of early stopping in callback

---
+++ examples/early_stopping_in_callback.jl
@@ -0,0 +1,20 @@
+using BlackBoxOptim
+
+rosenbrock(x) = sum( 100*( x[2:end] .- x[1:end-1].^2 ).^2 .+ ( x[1:end-1] .- 1 ).^2 )
+
+function myfitnessgoalachieved(oc)
+    best_fitness(oc) < 50.0
+end
+
+function cbearlystopping(oc)
+    if myfitnessgoalachieved(oc)
+        BlackBoxOptim.shutdown!(oc)
+    end
+end
+
+# We give max 1000 seconds to optimize but expect this to stop much earlier
+@time res = bboptimize(rosenbrock; SearchRange = (-100.0, 100.0), NumDimensions = 100, 
+        CallbackFunction = cbearlystopping, CallbackInterval = 0.0, MaxTime = 1000.0);
+
+@assert best_fitness(res) < 10.0
+@assert BlackBoxOptim.stop_reason(res) == "Run explicitly stopped via shutdown method"
