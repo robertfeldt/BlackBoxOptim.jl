d9f87f9bfe9cd85b8ea6d772948bd10a74881ac6
Alexey Stukalov <astukalov@gmail.com>
2023-06-18 17:26:11 -0700

    use `Test.@test_logs` macro
    instead of local `@test_warns`;
    one benefit is that test diagnostic output is more relevant

--- test/test_fitness.jl
+++ test/test_fitness.jl
@@ -1,16 +1,3 @@
-using Logging
-
-macro test_warns(expr)
-    quote
-        iob = IOBuffer()
-        with_logger(SimpleLogger(iob)) do
-            $(esc(expr))
-        end
-        flush(iob)
-        @test occursin(r"^┌ Warning:", String(take!(iob)))
-    end
-end
-
 @testset "Fitness" begin
     @testset "hat_compare() Float64" begin
         @test hat_compare(1.0, 2.0) == -1
@@ -255,12 +242,12 @@ end
 
         @testset "ϵ_index() warns when clamping large fitness values" begin
             # To address the issue reported here: https://github.com/robertfeldt/BlackBoxOptim.jl/issues/163
-            # We warn if the ϵ-box iindex has to be clamped since there are big risks with this, i.e.
+            # We warn if the ϵ-box index has to be clamped since there are big risks with this, i.e.
             # all fitness values that are large will end up in the same ϵ-box.
 
             u = float(typemax(Int)) * 2.0 # Ensure large enough it will overflow Int
-            @test_warns BlackBoxOptim.ϵ_index(u, 1.0, Val{true})
-            @test_warns BlackBoxOptim.ϵ_index(u, 1.0, Val{false})
+            @test_logs (:warn, r"^Clamping the epsilon-box index") BlackBoxOptim.ϵ_index(u, 1.0, Val{true})
+            @test_logs (:warn, r"^Clamping the epsilon-box index") BlackBoxOptim.ϵ_index(u, 1.0, Val{false})
 
             # But if the ϵ is higher we should be fine:
             idx, dist = BlackBoxOptim.ϵ_index(u, 10.0, Val{true})
